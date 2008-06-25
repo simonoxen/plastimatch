cdd3d1b6fb8173eed15c2adcafce335d11b3b41b
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2008-06-25 16:08:46 +0000

    Rough draft of adding slicer fiducials

--- plastimatch-slicer.cxx
+++ plastimatch-slicer.cxx
@@ -21,7 +21,9 @@
 </parameters>
 */
 
-const char* parms_fn = "C:/tmp/plmslc-tmp.parms";
+const char* parms_fn = "C:/tmp/plmslc-parms.txt";
+const char* ff_fn = "C:/tmp/plmslc-ff.txt";
+const char* mf_fn = "C:/tmp/plmslc-mf.txt";
 
 int main(int argc, char * argv [])
 {
@@ -33,10 +35,10 @@ int main(int argc, char * argv [])
 	     "fixed=%s\n"
 	     "moving=%s\n"
 	     "xf_out=%s\n"
-	     "vf_out=%s\n"
+//	     "vf_out=%s\n"
 	     "img_out=%s\n\n"
 	     "[STAGE]\n"
-	     "xform=bspline\n"
+	     "xform=bspline\n"	
 	     "optim=lbfgsb\n"
 	     "impl=gpuit_cpu\n"
 	     "max_its=100\n"
@@ -56,8 +58,20 @@ int main(int argc, char * argv [])
 	     plmslc_fixed_volume.c_str(),
 	     plmslc_moving_volume.c_str(),
 	     "C:/tmp/plmslc-xf.txt",
-	     "C:/tmp/plmslc-vf.mha",
+//	     "C:/tmp/plmslc-vf.mha",
 	     plmslc_warped_volume.c_str());
+    fclose (fp);
+
+
+    fp = fopen (ff_fn, "w");
+    fprintf (fp, "Fixed fiducials:\n");
+    for (int i = 0; i < plmslc_fixed_fiducials.size(); i++) {
+      fprintf (fp, "%g %g %g\n", 
+	       plmslc_fixed_fiducials[i][0],
+	       plmslc_fixed_fiducials[i][1],
+	       plmslc_fixed_fiducials[i][2]
+	       );
+    }
     fclose (fp);
 
     Registration_Parms regp;
