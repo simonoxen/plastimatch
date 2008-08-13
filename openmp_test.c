4fc8fa85a783773c1b33073b76f79bc39364bb7e
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2008-08-13 04:34:09 +0000

    Update instructions for building iwth MSYS

--- openmp_test.c
+++ openmp_test.c
@@ -1,25 +1,56 @@
 #include <omp.h>
 
-main ()  {
 
-int nthreads, tid;
-
-/* Fork a team of threads giving them their own copies of variables */
+void
+speedtest_ref (void)
+{
+    /* Run a simple speed test, to see how fast things run */
 #pragma omp parallel private(tid)
-  {
-
-  /* Obtain and print thread id */
-  tid = omp_get_thread_num();
-  printf("Hello World from thread = %d\n", tid);
-
-  /* Only master thread does this */
-  if (tid == 0) 
     {
-    nthreads = omp_get_num_threads();
-    printf("Number of threads = %d\n", nthreads);
-    }
 
-  }  /* All threads join master thread and terminate */
+	/* Obtain and print thread id */
+	tid = omp_get_thread_num();
+	printf("Hello World from thread = %d\n", tid);
 
+	/* Only master thread does this */
+	if (tid == 0) 
+	{
+	    nthreads = omp_get_num_threads();
+	    printf("Number of threads = %d\n", nthreads);
+	}
+
+    }  /* All threads join master thread and terminate */
+
+}
+
+void
+speedtest_openmp (void)
+{
+}
+
+int
+main (int argc, char* argv)
+{
+    int nthreads, tid;
+
+    /* Fork a team of threads giving them their own copies of variables */
+#pragma omp parallel private(tid)
+    {
+
+	/* Obtain and print thread id */
+	tid = omp_get_thread_num();
+	printf("Hello World from thread = %d\n", tid);
+
+	/* Only master thread does this */
+	if (tid == 0) 
+	{
+	    nthreads = omp_get_num_threads();
+	    printf("Number of threads = %d\n", nthreads);
+	}
+
+    }  /* All threads join master thread and terminate */
+
+
+    return 0;
 }
 
