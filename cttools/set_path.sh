5f766517ecefcba618cd4973e0bdff2664a2a963
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2008-09-27 03:33:42 +0000

    Update set_path.sh for cygwin

--- cttools/set_path.sh
+++ cttools/set_path.sh
@@ -1,1 +1,8 @@
-export PATH=$PATH:/home/gcs6/build/registration
+#! /bin/sh
+CNAME=`uname -n`
+if test "slumber"==$CNAME; then
+  export PATH=$PATH:/cygdrive/c/gcs6/build/plastimatch-cygwin
+else
+  export PATH=$PATH:/home/gcs6/build/registration
+fi
+
