b9f942721712e709849b12bf25dafadb6e5c5aed
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2009-03-18 17:35:02 +0000

    Add missing file

---
+++ cuda_probe_main.c
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "cuda_probe.h"
+
+int
+main (int argc, char* argv[])
+{
+    cuda_probe ();
+    return 0;
+}
