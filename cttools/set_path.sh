20773d25a699656f1633a47d948f7baaab971bc0
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2009-04-03 22:48:52 +0000

    First attempt at testing directory

--- cttools/set_path.sh
+++ cttools/set_path.sh
@@ -4,7 +4,7 @@ if test "slumber" = $CNAME; then
   export PATH=$PATH:/cygdrive/c/gcs6/build/plastimatch-cygwin
 else
   ## fantasy
-  export PATH=$PATH:/home/gcs6/build/plastimatch-release
+  export PATH=$PATH:/home/gcs6/build/plastimatch-3.10.2
   export PATH=$PATH:/home/gcs6/projects/plastimatch/cttools
 fi
 
