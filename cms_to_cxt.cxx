fa651151fc805a750b8bb18e2f3787c869d39fc4
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2009-05-19 16:26:54 +0000

    Slight progress to cms filenames

--- cms_to_cxt.cxx
+++ cms_to_cxt.cxx
@@ -5,6 +5,11 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+#include <algorithm>
+
+#include <itksys/SystemTools.hxx>
+#include <itksys/Directory.hxx>
+#include <itksys/RegularExpression.hxx>
 #include "itkDirectory.h"
 #include "itkRegularExpressionSeriesFileNames.h"
 
@@ -14,29 +19,99 @@ print_usage (void)
     printf ("Usage: cms_to_cxt directory output_file.cxt\n");
 }
 
+/* Modified from ITK source code, function RegularExpressionSeriesFileNames::
+   GetFileNames() */
+struct lt_pair_numeric_string_string
+{
+  bool operator()(const std::pair<std::string, std::string> s1, 
+                  const std::pair<std::string, std::string> s2) const
+    {
+    return atof(s1.second.c_str()) < atof(s2.second.c_str());
+    }
+};
+
+struct lt_pair_alphabetic_string_string
+{
+  bool operator()(const std::pair<std::string, std::string> s1, 
+                  const std::pair<std::string, std::string> s2) const
+    {
+    return s1.second < s2.second;
+    }
+};
+
+void
+get_file_names (std::vector<std::pair<std::string,std::string> > *file_names,
+		const char *input_dir, const char *regular_expression)
+{
+    int m_SubMatch = 1;
+    int m_NumericSort = 1;
+    std::string m_Directory = input_dir;
+    itksys::RegularExpression reg;
+
+    if (!reg.compile(regular_expression)) {
+	printf ("Error\n");exit (-1);
+    }
+
+    // Process all files in the directory
+    itksys::Directory fileDir;
+    if (!fileDir.Load (input_dir)) {
+	printf ("Error\n");exit (-1);
+    }
+
+    //    std::vector<std::pair<std::string,std::string> > sorted_filenames;
+
+    // Scan directory for files. Each file is checked to see if it
+    // matches the m_RegularExpression.
+    for (unsigned long i = 0; i < fileDir.GetNumberOfFiles(); i++)
+    {
+	// Only read files
+	if (itksys::SystemTools::FileIsDirectory( (m_Directory + "/" + fileDir.GetFile(i)).c_str() ))
+	{
+	    continue;
+	}
+
+	if (reg.find(fileDir.GetFile(i)))
+	{
+	    // Store the full filename and the selected sub expression match
+	    std::pair<std::string,std::string> fileNameMatch;
+	    fileNameMatch.first = m_Directory + "/" + fileDir.GetFile(i);
+	    fileNameMatch.second = reg.match(m_SubMatch);
+	    file_names->push_back(fileNameMatch);
+	}
+    }
+  
+    // Sort the files. The files are sorted by the sub match defined by
+    // m_SubMatch. Sorting can be alpahbetic or numeric.
+    if (m_NumericSort)
+    {
+	std::sort(file_names->begin(),
+		  file_names->end(),
+		  lt_pair_numeric_string_string());
+    }
+    else
+    {
+	std::sort(file_names->begin(),
+		  file_names->end(),
+		  lt_pair_alphabetic_string_string());
+    }
+}
+
 void
 do_cms_to_cxt (char *input_dir, char *output_fn)
 {
-    typedef itk::RegularExpressionSeriesFileNames NameGeneratorType;
-    NameGeneratorType::Pointer name_generator = NameGeneratorType::New ();
+    const char* filename_re = "T\\.([-\\.0-9]*)\\.WC";
 
-    /* Test make sure it is a directory */
-    itk::Directory::Pointer itk_dir = itk::Directory::New ();
-    if (!itk_dir->Load (input_dir)) {
-	printf ("Could not open input directory: %s\n", input_dir);
-	exit (-1);
-    }
+    /* Get the list of filenames */
+    std::vector<std::pair<std::string,std::string> > file_names;
+    get_file_names (&file_names, input_dir, filename_re);
 
-    /* Get file names matching pattern, they won't be sorted properly though */
-    name_generator->SetRegularExpression ("T\\.([-\\.0-9]*)\\.WC");
-    name_generator->SetSubMatch (0);
-    name_generator->SetDirectory (input_dir);
-    std::vector<std::string> input_files;
-    input_files = name_generator->GetFileNames ();
-
-    std::vector<std::string>::iterator it = input_files.begin();
-    while (it != input_files.end()) {
-	printf ("File: %s\n", (*it).c_str());
+    /* Iterate through filenames, adding data to CXT */
+    std::vector<std::pair<std::string,std::string> >::iterator it;
+    it = file_names.begin();
+    while (it != file_names.end()) {
+	printf ("File: %s, Loc: %s\n", 
+		(*it).first.c_str(),
+		(*it).second.c_str());
 	++it;
     }
 }
