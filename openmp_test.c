37f59334e615dbd318da5e1212f2664ed02251dd
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2009-07-02 00:47:06 +0000

    Add timing test to openmp test program

--- openmp_test.c
+++ openmp_test.c
@@ -1,8 +1,15 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
 #include "plm_config.h"
 #include <stdio.h>
+#include <time.h>
 #if (OPENMP_FOUND)
 #include <omp.h>
 #endif
+
+#define LOOP1 2000
+#define LOOP2 20000
 
 #if (OPENMP_FOUND)
 void
@@ -27,15 +34,33 @@ display_num_threads (void)
 }
 
 void
-speedtest_openmp (void)
+speedtest_openmp_1 (void)
 {
-#define LOOP1 100000
-#define LOOP2 100000
-    int i, j;
-    double d1, d2;
-    d1 = 0.0;
-    d2 = 1.0;
+    int i;
+#pragma omp parallel for
     for (i = 0; i < LOOP1; i++) {
+	int j;
+	double d1 = 0.0;
+	double d2 = 1.0;
+	for (j = 0; j < LOOP2; j++) {
+	    d1 += 0.872013;
+	    d2 *= d1;
+	    if (d2 > 1.0) {
+		d2 -= floor(d2);
+	    }
+	}
+    }
+}
+
+void
+speedtest_openmp_2 (void)
+{
+    int i;
+    double d1, d2;
+    for (i = 0; i < LOOP1; i++) {
+	int j;
+	double d1 = 0.0;
+	double d2 = 1.0;
 	for (j = 0; j < LOOP2; j++) {
 	    d1 += 0.872013;
 	    d2 *= d1;
@@ -50,9 +75,22 @@ speedtest_openmp (void)
 int
 main (int argc, char* argv)
 {
+    clock_t time_start, time_end;
 
 #if (OPENMP_FOUND)
     display_num_threads ();
+    time_start = clock ();
+    speedtest_openmp_1 ();
+    time_end = clock ();
+    printf ("Time = %f seconds\n", 
+	    ((float) (time_end - time_start)) / CLOCKS_PER_SEC);
+
+    time_start = clock ();
+    speedtest_openmp_2 ();
+    time_end = clock ();
+    printf ("Time = %f seconds\n", 
+	    ((float) (time_end - time_start)) / CLOCKS_PER_SEC);
+
 #else
     printf ("Sorry, openmp was not supported by your compiler\n");
 #endif
