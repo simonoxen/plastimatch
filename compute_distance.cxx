469301263d6faf4874024399471517ffda1fc872
Marta Peroni <m.peroni@gmail.com>
2009-07-16 07:15:28 +0000

    added necessary files for distance computation

---
+++ compute_distance.cxx
@@ -0,0 +1,120 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "compute_distance.h"
+
+void print_usage (void)
+{
+	printf ("Usage: contour_statistics \n");
+	printf ("  mode (options: cp, cm)  ");
+	printf ("  file1  ");
+	printf ("  file2  ");
+	printf ("  [filename]\n");
+	printf ("  OPTIONS EXPLANATION: \n");
+	printf ("  cp= Closest Point computation between mesh file and reference points: file1 *.obj file, file2 *.txt file with the reference points, filename output *.txt file\n\n\n");
+	printf ("  cm= Closest Mesh computation: file1 *.obj file with reference mesh, file2 *.obj file with the other mesh, filename output *.txt file\n");
+	exit (-1);
+}
+
+int main(int argc, char* argv[])
+{
+	ImgType::Pointer reference=ImgType::New();
+	ImgType::Pointer warped=ImgType::New();
+	ImgType::Pointer ex_1=ImgType::New();
+	ImgType::Pointer ex_2=ImgType::New();
+	ImgType::Pointer ex_3=ImgType::New();
+	FILE* mesh;
+	//FILE* refMesh;
+	FILE* MDpoints;
+	FILE* output;
+
+	if (argc<3)
+		print_usage();
+
+	if(strcmp("cp",argv[1])==0){
+		mesh=fopen(argv[2],"r");
+		MDpoints=fopen(argv[3],"r");
+		if(!mesh || !MDpoints){
+			fprintf(stderr,"Error: could not open the files for the cp calculation for reading!\n");
+			if(!mesh)
+				fprintf(stderr,"This file could not be opened: %s\n",argv[2]);
+			else
+				fprintf(stderr,"This file could not be opened: %s\n",argv[3]);
+			exit(-1);
+		}
+	}else if (strcmp("cm",argv[1])==0){
+		printf("Sorry! there is some work going on here!");
+		exit(-1);
+		//refMesh=fopen(argv[2],"r");
+		//mesh=fopen(argv[3],"r");
+		//if(!refMesh || !mesh){
+		//	fprintf(stderr,"Error: could not open the files for the cp calculation for reading!\n");
+		//	if(!refMesh)
+		//		fprintf(stderr,"This file could not be opened: %s\n",argv[2]);
+		//	else
+		//		fprintf(stderr,"This file could not be opened: %s\n",argv[3]);
+		//	exit(-1);
+		//}
+	}else{
+		fprintf(stderr,"Sorry! you typed in the wrong mode");
+		exit(-1);
+	}
+
+	if (argc<5){
+		if(strcmp("cp",argv[1])==0){
+			output=fopen("cp_dist.txt","w");
+		}else{
+			output=fopen("mesh_dist.txt","w");
+			fclose(output); //to be canceled when mesh mesh distnace is implemented
+		}
+	}else if (argc==5){
+		output=fopen(argv[4],"w");
+	}
+	if(!output){
+		fprintf(stderr, "An error occurred while opening the file for writing the outputs!");
+		exit(-1);
+	}
+
+	if(strcmp("cp",argv[1])==0){
+
+		SURFACE* surface=(SURFACE*)malloc(sizeof(SURFACE));
+		memset(surface,0,sizeof(SURFACE));
+		printf("Allocated Surface\n");
+
+		do_cp(mesh,MDpoints,surface,output);
+
+	}else if(strcmp("cm",argv[1])==0){
+		printf("function in development\n");
+		exit(-1);
+	}
+	return 0;
+}
+
+
+
+
+		//TRIANGLE_LIST* triangles=(TRIANGLE_LIST*)malloc(sizeof(TRIANGLE_LIST));
+		//memset(triangles,0,sizeof(TRIANGLE_LIST));
+		//triangles->num_triangles=0;
+		//triangles=&surface->triangles;
+		//printf("LAST: %d %d %d\n",triangles->first[triangles->num_triangles-1],
+		//	triangles->second[triangles->num_triangles-1], triangles->third[triangles->num_triangles-1]);
+
+		//MASS* center_mass=(MASS*)malloc(sizeof(MASS));
+		//memset(center_mass,0,sizeof(MASS));
+		//center_mass->num_triangles=0;
+		//center_mass->x=(float*)malloc(sizeof(float));
+		//memset(center_mass->x,0,sizeof(float));
+		//center_mass->y=(float*)malloc(sizeof(float));
+		//memset(center_mass->y,0,sizeof(float));
+		//center_mass->z=(float*)malloc(sizeof(float));
+		//memset(center_mass->z,0,sizeof(float));
+		//center_mass->corrpoint_index=(int*)malloc(sizeof(int));
+		//memset(center_mass->corrpoint_index,0,sizeof(int));
+
+		//for(int r=0; r<center_mass->num_triangles; r++)
+		//	printf("CORR: %d\n",center_mass->corrpoint_index[r]);
+
+
+
+		//fclose(mesh);
---
+++ compute_distance.h
@@ -0,0 +1,84 @@
+/*===========================================================
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+===========================================================*/
+#ifndef _contour_statistics_h
+#define _contour_statistics_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "plm_config.h"
+#include "itkImageFileReader.h"
+#include "itkImage.h"
+#include "itk_image.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkImageSliceConstIteratorWithIndex.h"
+//#include "slice_extract.h"
+
+
+/* =======================================================================*
+    Definitions
+ * =======================================================================*/
+typedef UCharImageType ImgType;
+typedef itk::Image<unsigned char, 2>	intImgType;
+
+typedef struct vertices VERTICES_LIST;
+struct vertices {
+    int num_vertices;
+	float* x;
+	float* y;
+	float* z;
+};
+
+typedef struct triangle TRIANGLE_LIST;
+struct triangle {
+    int num_triangles;
+	int* first;
+	int* second;
+	int* third;
+};
+
+typedef struct mass MASS;
+struct mass {
+	float* x;
+	float* y;
+	float* z;
+	int num_triangles;
+	
+};
+
+typedef struct corr CORR;
+struct corr{
+	int* corrpoint_index;
+	int num_points;
+};
+
+typedef struct plane PLANE;
+struct plane {
+	int num_planes;
+	float* a0;
+	float* a1;
+	float* a2;
+	float* a3;
+};
+
+typedef struct surface SURFACE;
+struct surface{
+	VERTICES_LIST vertices;
+	TRIANGLE_LIST triangles;
+	MASS centres;
+	VERTICES_LIST MDpoints;
+	CORR correspondance;
+	PLANE planes;
+};
+
+void do_cp(FILE* mesh,FILE* MDpoints, SURFACE* surface, FILE* output);
+void read_obj(FILE* mesh, SURFACE* surface);
+void read_MDcontours(FILE* MDpoints, SURFACE* surface);
+void calculate_mass(SURFACE* surface);
+void cp(SURFACE* surface);
+void compute_plane(SURFACE* surface);
+
+#endif
