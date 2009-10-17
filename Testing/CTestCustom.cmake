8ddeedcff7c878dcc2f958775f9bbc55485fdd08
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2009-10-17 16:01:10 +0000

    Add more regression tests for bspline

--- Testing/CTestCustom.cmake
+++ Testing/CTestCustom.cmake
@@ -1,4 +1,7 @@
 
+#SET (REDUCED_TEST ON)
+SET (REDUCED_TEST OFF)
+
 IF (NOT CUDA_FOUND)
   SET (CTEST_CUSTOM_TESTS_IGNORE
     ${CTEST_CUSTOM_TESTS_IGNORE}
@@ -6,3 +9,29 @@ IF (NOT CUDA_FOUND)
     "plastimatch-bspline-cuda" 
     )
 ENDIF (NOT CUDA_FOUND)
+
+IF (REDUCED_TEST)
+  SET (CTEST_CUSTOM_TESTS_IGNORE
+    ${CTEST_CUSTOM_TESTS_IGNORE}
+    "synth-test-1"
+    "synth-test-2"
+    "bspline-bxf"
+    "bspline-bxf-check"
+    "bspline-a"
+    "bspline-b"
+    "bspline-c"
+    "bspline-c-check"
+    "bspline-d"
+    "bspline-e"
+    "bspline-f"
+    "bspline-f-check"
+    "drr"
+    "fdk-cpu"
+    "fdk-cuda"
+    "plastimatch-itk-translation"
+    "plastimatch-bspline-single-c" 
+    "plastimatch-bspline-single-f" 
+    "plastimatch-bspline-openmp" 
+    "xf-to-xf"
+    )
+ENDIF (REDUCED_TEST)
