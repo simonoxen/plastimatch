d9e093535c859997566b72e6f125a648670f43fd
Rui Li <rli4@partners.org>
2009-12-09 16:46:08 +0000

    add function to generate dicom data from mha.

--- mha_to_dicom.cxx
+++ mha_to_dicom.cxx
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------
    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
    ----------------------------------------------------------------------- */
-/* This program is used to convert from dicom to mha */
+/* This program is used to convert from mha to dicom */
 #include <time.h>
 #include "plm_config.h"
 #include "itkImage.h"
@@ -12,14 +12,17 @@ main(int argc, char *argv[])
 {
     if (argc != 3 && argc != 4) {
 	std::cerr << "Usage: " << argv[0];
-	std::cerr << " input_dir output_filename [output_type]" << std::endl;
+	std::cerr << " input_filename output_dir [output_type]" << std::endl;
 	std::cerr << " output_type can be: uchar, short, ushort, or float" << std::endl;
 	return 1;
     }
 
     int output_type = 0;
-    char* dicom_dir = argv[1];
-    char* output_fn = argv[2];
+    // input file name
+    char* input_fn = argv[1];
+    // output to dicom directory
+    char* dicom_dir = argv[2];
+    
     if (argc == 4) {
 	if (0 == strcmp(argv[3], "uchar")) {
 	    output_type = PLM_IMG_TYPE_ITK_UCHAR;
@@ -34,23 +37,25 @@ main(int argc, char *argv[])
 	    exit (1);
 	}
     } else {
-	output_type = PLM_IMG_TYPE_ITK_SHORT;
+        output_type = PLM_IMG_TYPE_ITK_SHORT;
     }
 
     if (output_type == PLM_IMG_TYPE_ITK_UCHAR) {
-        UCharImageType::Pointer input_image = load_uchar (dicom_dir, 0);
-        itk_image_save (input_image, output_fn);
+        UCharImageType::Pointer input_image = load_uchar (input_fn, 0);
+        itk_image_save_short_dicom (input_image, dicom_dir);
     } else if (output_type == PLM_IMG_TYPE_ITK_SHORT) {
-        ShortImageType::Pointer input_image = load_short (dicom_dir, 0);
-        itk_image_save (input_image, output_fn);
+        ShortImageType::Pointer input_image = load_short (input_fn, 0);
+        itk_image_save_short_dicom (input_image, dicom_dir);
     } else if (output_type == PLM_IMG_TYPE_ITK_USHORT) {
-	    UShortImageType::Pointer input_image = load_ushort (dicom_dir, 0);
-	    itk_image_save (input_image, output_fn);
+        UShortImageType::Pointer input_image = load_ushort (input_fn, 0);
+        itk_image_save_short_dicom (input_image, dicom_dir);
     } else if (output_type == PLM_IMG_TYPE_ITK_FLOAT) {
-	    FloatImageType::Pointer input_image = load_float (dicom_dir, 0);
-    	itk_image_save (input_image, output_fn);
+        FloatImageType::Pointer input_image = load_float (input_fn, 0);
+        itk_image_save_short_dicom (input_image, dicom_dir);
     }
 
+
+    
     printf ("Finished!\n");
     return 0;
 }
