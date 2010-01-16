26d55d8627bffda10c3ab7c852f2d205b995d750
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-01-16 18:57:53 +0000

    Connect pqt to sqlite database (memory)

--- pqt_main_window.cxx
+++ pqt_main_window.cxx
@@ -4,6 +4,9 @@
 #include "plm_config.h"
 #include <stdio.h>
 #include <QtGui>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+
 #include "pqt_data_source_dialog.h"
 #include "pqt_main_window.h"
 
@@ -18,12 +21,44 @@ Pqt_main_window::Pqt_main_window ()
     /* Attach model to QT table in main window */
     m_patient_list_model = new Pqt_patient_list_model;
     tableView->setModel (m_patient_list_model);
+
+    /* Set path to persistent application settings */
+    QCoreApplication::setOrganizationName ("Plastimatch");
+    QCoreApplication::setOrganizationDomain ("plastimatch.org");
+    QCoreApplication::setApplicationName ("plastimatch_qt");
+
+    /* Load application settings */
+    QSettings settings;
+    QString db_path = settings.value ("db/sqlite3_path", 
+	"/tmp/pqt.sqlite").toString();
+
+    test_database ();
 }
 
 Pqt_main_window::~Pqt_main_window ()
 {
     delete m_data_source_dialog;
     delete m_patient_list_model;
+
+    QSettings settings;
+    settings.sync ();
+}
+
+void
+Pqt_main_window::test_database ()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase ("QSQLITE");
+    db.setDatabaseName (":memory:");
+    bool ok = db.open();
+    if (!ok) {
+	QSqlError qsqlerror = db.lastError();
+
+	QMessageBox::information (this, QString ("Database error"),
+	    QString ("Error (%1,%2), %3")
+	    .arg(qsqlerror.type())
+	    .arg(qsqlerror.number())
+	    .arg(qsqlerror.text()));
+    }
 }
 
 void
