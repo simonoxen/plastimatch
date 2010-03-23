a4f1f56a5d50eb5c928809a03c3768a7a7c618c1
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-03-23 13:45:38 +0000

    Add capability to export geometry matrix from hnd_to_pfm

--- hnd_to_pfm.c
+++ hnd_to_pfm.c
@@ -4,6 +4,8 @@
 #include "plm_config.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "file_util.h"
 #include "hnd_io.h"
 #include "plm_int.h"
 #include "print_and_exit.h"
@@ -12,9 +14,8 @@
 int 
 main (int argc, char* argv[]) 
 {
-    FILE *fp_pfm;
     Proj_image *proj;
-    char *hnd_fn, *pfm_fn;
+    char *hnd_fn, *pfm_fn, *mat_fn, *tmp;
 
     if (argc != 3) {
 	printf ("Usage: hndtopfm hndfile pfmfile\n");
@@ -24,20 +25,21 @@ main (int argc, char* argv[])
     hnd_fn = argv[1];
     pfm_fn = argv[2];
 
+    /* Create filename for matrix file */
+    tmp = strdup (pfm_fn);
+    strip_extension (tmp);
+    mat_fn = (char*) malloc (strlen (tmp) + 5);
+    sprintf (mat_fn, "%s.txt", tmp);
+    free (tmp);
+
     /* Read image */
     proj = proj_image_load (hnd_fn, 0);
     if (!proj) {
 	print_and_exit ("Couldn't load file for read: %s\n", hnd_fn);
     }
 
-    /* Write image */
-    fp_pfm = fopen (pfm_fn, "wb");
-    if (!fp_pfm) {
-	printf ("Error, cannot open file %s for write\n", pfm_fn);
-	exit (1);
-    }
-    fprintf (fp_pfm, "Pf\n%d %d\n-1\n", proj->dim[0], proj->dim[1]);
-    fwrite (proj->img, sizeof(float), proj->dim[0] * proj->dim[1], fp_pfm);
-    fclose (fp_pfm);
+    /* Write image and header */
+    proj_image_save (proj, pfm_fn, mat_fn);
+
     return 0;
 }
