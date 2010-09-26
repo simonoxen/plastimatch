bdcb1198631121dd72e874521399ecaa0f469d76
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-09-26 20:42:12 +0000

    Add mimgw build scripts

---
+++ cttools/mingw_itk.sh
@@ -0,0 +1,10 @@
+/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake \
+	-G"MSYS Makefiles" \
+	-DBUILD_EXAMPLES=OFF \
+	-DBUILD_TESTING=OFF \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_USE_PTHREADS=OFF \
+	-DITK_USE_REVIEW=ON \
+	-DITK_USE_OPTIMIZED_REGISTRATION_METHODS=ON \
+	/c/gcs6/build/src/InsightToolkit-3.20.0/
+
---
+++ cttools/mingw_plastimatch.sh
@@ -0,0 +1,6 @@
+/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake \
+	-G"MSYS Makefiles" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DITK_DIR=/c/gcs6/build/itk-3.20.0-mingw \
+	/c/gcs6/projects/plastimatch
+
