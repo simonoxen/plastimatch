ce05d1ad43dc1c10d28cede95af5be485a2c2de2
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-09-28 05:22:37 +0000

    Fix yet another f2c bug

--- plm_fortran.h.cmake
+++ plm_fortran.h.cmake
@@ -4,6 +4,7 @@
 #ifndef _plm_fortran_h_
 #define _plm_fortran_h_
 
+#cmakedefine PLM_USE_F2C
 #cmakedefine PLM_USE_INCLUDED_F2C
 
 #if defined (_WIN32)
@@ -11,18 +12,20 @@
 
 #else /* UNIX */
 
-  #if defined (PLM_USE_INCLUDED_F2C)
-     /* Included f2c. */
-     #include "@CMAKE_SOURCE_DIR@/libs/libf2c/f2c.h"
-  #else 
-     /* System or self-built f2c.
-        If you try to compile and link against a self-built f2c library 
-        without installing, you can't include the f2c directory because 
-        f2c includes a broken "ctype.h" which conflicts with the system one. 
-	Therefore, we use a full path here.
-	*/
-    #include "@F2C_INCLUDE_DIR@/f2c.h"
+  #if defined (PLM_USE_F2C)
+    #if defined (PLM_USE_INCLUDED_F2C)
+       /* Included f2c. */
+       #include "@CMAKE_SOURCE_DIR@/libs/libf2c/f2c.h"
+    #else 
+       /* System or self-built f2c.
+          If you try to compile and link against a self-built f2c library 
+          without installing, you can't include the f2c directory because 
+          f2c includes a broken "ctype.h" which conflicts with the system one. 
+	  Therefore, we use a full path here.
+	  */
+      #include "@F2C_INCLUDE_DIR@/f2c.h"
+    #endif
   #endif
-#endif
+#endif /* UNIX */
 
 #endif
