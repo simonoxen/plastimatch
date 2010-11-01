2945225bbf5bde8b45375f51bd820631c7fe9e3a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-11-01 02:24:47 +0000

    Fix compile for shared lib on windows

--- opencl_probe.h
+++ opencl_probe.h
@@ -4,10 +4,13 @@
 #ifndef _opencl_probe_h_
 #define _opencl_probe_h_
 
+#include "plm_config.h"
+
 #if defined __cplusplus
 extern "C" {
 #endif
 
+gpuit_EXPORT
 int opencl_probe (void);
 
 #if defined __cplusplus
