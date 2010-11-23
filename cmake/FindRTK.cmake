f8f1b60e9c22e3e4464cd67a9e75071c0d4435ec
James A Shackleford <shack@linux.com>
2010-11-23 19:39:47 +0000

    Now systems without ITK will not attempt to build RTK.

--- cmake/FindRTK.cmake
+++ cmake/FindRTK.cmake
@@ -16,7 +16,18 @@ IF (NOT RTK_SOURCE_DIR)
   MESSAGE (ERROR "Sorry, I couldn't find the RTK directory")
 ENDIF (NOT RTK_SOURCE_DIR)
 
-ADD_SUBDIRECTORY (${RTK_SOURCE_DIR})
+# JAS 2010.11.23
+# Make life easier for people who only
+# want to build plastimatch components
+# that don't depend on ITK.
+FIND_PACKAGE (ITK)
+
+IF (NOT ITK_FOUND)
+    MESSAGE (STATUS "ITK not found, omitting RTK")
+ELSE(NOT ITK_FOUND)
+    MESSAGE (STATUS "ITK found, including RTK")
+    ADD_SUBDIRECTORY (${RTK_SOURCE_DIR})
+ENDIF (NOT ITK_FOUND)
 
 IF (NOT RTK_DIR)
   FIND_PATH (RTK_DIR RTKconfig.cmake
@@ -24,7 +35,7 @@ IF (NOT RTK_DIR)
     DOC "directory containing RTK build files")
 ENDIF (NOT RTK_DIR)
 
-IF (RTK_DIR)
+IF (RTK_DIR AND ITK_FOUND)
   SET (RTK_FOUND 1)
   INCLUDE (${RTK_DIR}/RTKconfig.cmake)
-ENDIF (RTK_DIR)
+ENDIF (RTK_DIR AND ITK_FOUND)
