e77d1cd057695679172372d08af259f6ed1c1892
James A Shackleford <shack@linux.com>
2010-11-24 17:59:27 +0000

    Add CMake build option PLM_BUILD_RTK.

--- cmake/FindRTK.cmake
+++ cmake/FindRTK.cmake
@@ -3,6 +3,12 @@
 #
 #  RTK_SOURCE_DIR  - where to find RTK source directory
 
+# JAS 2010.11.24
+# I've grown tired for getting my build log filled with
+# errors from RTK when I check for Windows compile sanity...
+# Give an option to turn this off without excluding ITK.
+SET (PLM_BUILD_RTK ON CACHE BOOL "Build RTK")
+
 IF (NOT RTK_SOURCE_DIR)
   FIND_PATH (RTK_SOURCE_DIR RTKconfig.cmake.in
     "${CMAKE_SOURCE_DIR}/libs/RTK.git"
@@ -22,12 +28,19 @@ ENDIF (NOT RTK_SOURCE_DIR)
 # that don't depend on ITK.
 FIND_PACKAGE (ITK)
 
-IF (NOT ITK_FOUND)
-    MESSAGE (STATUS "ITK not found, omitting RTK")
-ELSE(NOT ITK_FOUND)
-    MESSAGE (STATUS "ITK found, including RTK")
-    ADD_SUBDIRECTORY (${RTK_SOURCE_DIR})
-ENDIF (NOT ITK_FOUND)
+
+IF (PLM_BUILD_RTK)
+    IF (NOT ITK_FOUND)
+        MESSAGE (STATUS "NOT building RTK, could not find ITK")
+    ELSE(NOT ITK_FOUND)
+        MESSAGE (STATUS "Building RTK")
+        ADD_SUBDIRECTORY (${RTK_SOURCE_DIR})
+    ENDIF (NOT ITK_FOUND)
+ELSE (PLM_BUILD_RTK)
+    MESSAGE (STATUS "NOT Building RTK")
+ENDIF (PLM_BUILD_RTK)
+
+
 
 IF (NOT RTK_DIR)
   FIND_PATH (RTK_DIR RTKconfig.cmake
@@ -35,7 +48,7 @@ IF (NOT RTK_DIR)
     DOC "directory containing RTK build files")
 ENDIF (NOT RTK_DIR)
 
-IF (RTK_DIR AND ITK_FOUND)
+IF (RTK_DIR AND ITK_FOUND AND PLM_BUILD_RTK)
   SET (RTK_FOUND 1)
   INCLUDE (${RTK_DIR}/RTKconfig.cmake)
-ENDIF (RTK_DIR AND ITK_FOUND)
+ENDIF (RTK_DIR AND ITK_FOUND AND PLM_BUILD_RTK)
