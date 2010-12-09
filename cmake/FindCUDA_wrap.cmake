97a0a4bd92ea6bd07aa8add6b46664f1575d750d
James A Shackleford <shack@linux.com>
2010-12-09 21:13:32 +0000

    Added PLM_CUDA_ALL_DEVICES flag to speed up "non-Release" builds (default=off)

--- cmake/FindCUDA_wrap.cmake
+++ cmake/FindCUDA_wrap.cmake
@@ -21,16 +21,21 @@ ENDIF(CUDA_FOUND)
 # JAS 08.25.2010
 #   Check to make sure nvcc has gcc-4.3 for compiling.
 #   This script will modify CUDA_NVCC_FLAGS if system default is not gcc-4.3
-#INCLUDE (nvcc-check.cmake)
 INCLUDE (nvcc-check)
 
-# JAS 08.25.2010
-#   Build code for all known compute capabilities
-SET (CUDA_NVCC_FLAGS
-  ${CUDA_NVCC_FLAGS}
-  -gencode arch=compute_10,code=sm_10
-  -gencode arch=compute_11,code=sm_11
-  -gencode arch=compute_12,code=sm_12
-  -gencode arch=compute_13,code=sm_13
-  -gencode arch=compute_20,code=sm_20
-  )
+# JAS 2010.12.09
+#   Build code for all known compute capabilities only if desired.
+SET (PLM_CUDA_ALL_DEVICES OFF CACHE BOOL "Generate GPU code for all compute capabilities?")
+
+IF (PLM_CUDA_ALL_DEVICES)
+    MESSAGE (STATUS "CUDA Build Level: ALL Compute Capabilities")
+    SET (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}
+        -gencode arch=compute_10,code=sm_10
+        -gencode arch=compute_11,code=sm_11
+        -gencode arch=compute_12,code=sm_12
+        -gencode arch=compute_13,code=sm_13
+        -gencode arch=compute_20,code=sm_20
+    )
+ELSE (PLM_CUDA_ALL_DEVICES)
+    MESSAGE (STATUS "CUDA Build Level: Build system Compute Capability ONLY!")
+ENDIF (PLM_CUDA_ALL_DEVICES)
