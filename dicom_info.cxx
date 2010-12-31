79ab21c09ef229ca519f292e2d3f0c0a2a38b5a6
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2010-12-31 16:37:44 +0000

    Remove compile warning on windows

--- dicom_info.cxx
+++ dicom_info.cxx
@@ -1,6 +1,7 @@
 /* -----------------------------------------------------------------------
    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
    ----------------------------------------------------------------------- */
+#include "plm_config.h"
 #include "itkImageFileReader.h"
 #include "itkGDCMImageIO.h"
 #include "itkImageIOBase.h"
