bf0c1eb475313dc2f6e24ee5f5bccb3e6c3ca0b9
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-01-01 18:05:06 +0000

    Add search for dlib library

---
+++ cmake/FindDlib.cmake
@@ -0,0 +1,32 @@
+# - Find Dlib
+# Find the native Dlib includes and library
+#
+#  DLIB_INCLUDE_DIR - where to find zlib.h, etc.
+#  DLIB_LIBRARIES   - List of libraries when using zlib.
+#  DLIB_FOUND       - True if zlib found.
+
+
+IF (DLIB_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET (Dlib_FIND_QUIETLY TRUE)
+ENDIF (DLIB_INCLUDE_DIR)
+
+FIND_PATH(DLIB_INCLUDE_DIR "dlib/algs.h")
+
+SET (DLIB_NAMES dlib)
+FIND_LIBRARY (DLIB_LIBRARY NAMES ${DLIB_NAMES})
+
+# handle the QUIETLY and REQUIRED arguments and set DLIB_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (DLIB DEFAULT_MSG 
+  DLIB_LIBRARY 
+  DLIB_INCLUDE_DIR)
+
+IF(DLIB_FOUND)
+  SET (DLIB_LIBRARIES ${DLIB_LIBRARY})
+ELSE (DLIB_FOUND)
+  SET (DLIB_LIBRARIES)
+ENDIF (DLIB_FOUND)
+
+MARK_AS_ADVANCED (DLIB_LIBRARY DLIB_INCLUDE_DIR)
