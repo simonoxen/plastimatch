f1df66b77ec3586ba9134e63d900e6f32f577c0e
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-01-01 18:30:16 +0000

    Skeleton code for dlib_test

---
+++ dlib_test.cxx
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "plm_config.h"
+#include <stdio.h>
+#include "dlib/algs.h"
+
+int
+main (int argc, char* argv[])
+{
+}
