780747ed5e85c9a99cd52b8dcf9105efd17a1e2b
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-01-02 18:59:25 +0000

    First working version of dlib_test.cxx

--- dlib_test.cxx
+++ dlib_test.cxx
@@ -19,13 +19,13 @@ using namespace dlib;
 
 int main()
 {
-    typedef std::map<unsigned long, double> sample_type;
-    std::vector<sample_type> samples;
+    typedef std::map<unsigned long, double> sparse_sample_type;
+    std::vector<sparse_sample_type> sparse_samples;
     std::vector<double> labels;
 
     load_libsvm_formatted_data (
         "/home/gsharp/Dropbox/autolabel/t-spine/t-spine.libsvm.all.scale", 
-	samples, 
+	sparse_samples, 
 	labels
     );
 
@@ -57,23 +57,12 @@ int main()
     }
 #endif
 
-    std::vector<matrix<sample_type::value_type::second_type,0,1> > 
-	dense_matrix;
-    dense_matrix = sparse_to_dense (samples);
+    typedef matrix<sparse_sample_type::value_type::second_type,0,1> 
+	dense_sample_type;
+    std::vector<dense_sample_type> dense_samples;
+    dense_samples = sparse_to_dense (sparse_samples);
 
-#if defined (commentout)
-    typedef radial_basis_kernel<sample_type> kernel_type;
-    krr_trainer<kernel_type> trainer;
-#endif
-
-#if defined (commentout)
-    // Now we are making a typedef for the kind of kernel we want to use.  I picked the
-    // radial basis kernel because it only has one parameter and generally gives good
-    // results without much fiddling.
-    typedef radial_basis_kernel<sample_type> kernel_type;
-
-    // Here we declare an instance of the krr_trainer object.  This is the
-    // object that we will later use to do the training.
+    typedef radial_basis_kernel<dense_sample_type> kernel_type;
     krr_trainer<kernel_type> trainer;
 
     // Here we set the kernel we want to use for training.   The radial_basis_kernel 
@@ -81,51 +70,21 @@ int main()
     // gamma to try is 1.0/(mean squared distance between your sample points).  So 
     // below we are using a similar value computed from at most 2000 randomly selected
     // samples.
-    const double gamma = 3.0/compute_mean_squared_distance(randomly_subsample(samples, 2000));
+    const double gamma = 3.0 / compute_mean_squared_distance(randomly_subsample(dense_samples, 2000));
     cout << "using gamma of " << gamma << endl;
     trainer.set_kernel(kernel_type(gamma));
 
+#if defined (commentout)
     // now train a function based on our sample points
-    decision_function<kernel_type> test = trainer.train(samples, labels);
-
-    // now we output the value of the sinc function for a few test points as well as the 
-    // value predicted by our regression.
-    m(0) = 2.5; cout << sinc(m(0)) << "   " << test(m) << endl;
-    m(0) = 0.1; cout << sinc(m(0)) << "   " << test(m) << endl;
-    m(0) = -4;  cout << sinc(m(0)) << "   " << test(m) << endl;
-    m(0) = 5.0; cout << sinc(m(0)) << "   " << test(m) << endl;
-
-    // The output is as follows:
-    //using gamma of 0.075
-    //    0.239389   0.239389
-    //    0.998334   0.998362
-    //    -0.189201   -0.189254
-    //    -0.191785   -0.186618
-
-    // The first column is the true value of the sinc function and the second
-    // column is the output from the krr estimate.  
-
+    decision_function<kernel_type> test = trainer.train(dense_samples, labels);
+#endif
 
     // Note that the krr_trainer has the ability to tell us the leave-one-out cross-validation
     // accuracy.  The train() function has an optional 3rd argument and if we give it a double
     // it will give us back the LOO error.
     double loo_error;
-    trainer.train(samples, labels, loo_error);
+    trainer.train(dense_samples, labels, loo_error);
     cout << "mean squared LOO error: " << loo_error << endl;
     // Which outputs the following:
     // mean squared LOO error: 8.29563e-07
-
-
-
-
-    // Another thing that is worth knowing is that just about everything in dlib is serializable.
-    // So for example, you can save the test object to disk and recall it later like so:
-    ofstream fout("saved_function.dat",ios::binary);
-    serialize(test,fout);
-    fout.close();
-
-    // now lets open that file back up and load the function object it contains
-    ifstream fin("saved_function.dat",ios::binary);
-    deserialize(test, fin);
-#endif
 }
