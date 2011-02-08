489fc9d9c6fd4ee7acc112d80091545e1c3ecb0d
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-02-08 16:35:26 +0000

    Autolabel script can now generate ASCII data for use by matlab

--- extra/autolabel/t_spine.pl
+++ extra/autolabel/t_spine.pl
@@ -6,9 +6,12 @@ use File::Spec::Functions;
 
 #$dropbox_dir = "c:/gcs6/My Dropbox";
 $dropbox_dir = "/PHShome/gcs6/Dropbox";
-$in_base = "$dropbox_dir/autolabel/t-spine-in/rider-lung";
-#$in_base = "$dropbox_dir/autolabel/t-spine-in/rider-pilot";
+#$in_base = "$dropbox_dir/autolabel/t-spine-in/rider-lung";
+$in_base = "$dropbox_dir/autolabel/t-spine-in/rider-pilot";
 $out_base = "$dropbox_dir/autolabel/t-spine-2";
+
+$make_thumbnails = 0;
+$make_reference_curves = 1;
 
 sub parse_fcsv {
     ($fn) = @_;
@@ -44,14 +47,30 @@ for (@list){
 
    copy ($fcsv_fn,$out_fcsv);
 
+   if ($make_reference_curves) {
+       $out_fn = $out_fcsv;
+       $out_fn =~ s/fcsv$/txt/;
+       open OF, ">$out_fn";
+   }
+
    @locs = parse_fcsv ($fcsv_fn);
    for $loc (@locs) {
        ($label,$z) = split (/,/, $loc, 2);
-       $out_fn = $out_fn_base . "_${label}.mhd";
-       $cmd = "plastimatch thumbnail "
-	 . "--input \"$img_in_fn\" --output \"$out_fn\" "
-	 . "--thumbnail-dim 16 --thumbnail-spacing 25.0 --slice-loc $z";
-       print "$cmd\n";
-       print `$cmd`;
+       if ($make_thumbnails) {
+	   $out_fn = $out_fn_base . "_${label}.mhd";
+	   $cmd = "plastimatch thumbnail "
+	     . "--input \"$img_in_fn\" --output \"$out_fn\" "
+	     . "--thumbnail-dim 16 --thumbnail-spacing 25.0 --slice-loc $z";
+	   print "$cmd\n";
+	   print `$cmd`;
+       }
+       if ($make_reference_curves) {
+	   $label_loc = $label;
+	   $label_loc =~ s/P//;
+	   print OF "$label_loc $z\n";
+       }
+   }
+   if ($make_reference_curves) {
+       close OF;
    }
 }
