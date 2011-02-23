b39c90a79fb72145df3ac39d28fe27d48f55edd7
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-02-23 12:45:03 +0000

    Skeleton code for slicer synthetic image generator

---
+++ plastimatch_slicer_synth.cxx
@@ -0,0 +1,15 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "plm_config.h"
+#include "plastimatch_slicer_synthCLP.h"
+
+int 
+main (int argc, char * argv [])
+{
+    PARSE_ARGS;
+
+    /* Do something */
+
+    return EXIT_SUCCESS;
+}
