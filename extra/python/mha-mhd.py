6e8e37f17234f06cd7e8256583d4503cdc53668d
Paolo Zaffino <p.zaffino@yahoo.it>
2011-03-22 15:27:20 +0000

    add python folder, mha.py and mha-mhd.py

---
+++ extra/python/mha-mhd.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+## rev 2
+## Usage: Unix based OS: ./mha-mhd.py file_to_convert
+##        Windows: python mha-mhd.py file_to_convert
+##
+## This script converts a mha file in a mhd file and vice versa.
+## The input file can be an image or a vector field.
+##
+## Author: Paolo Zaffino (p.zaffino@yahoo.it)
+##
+## NOT TESTED ON PYTHON 3
+
+import sys
+
+file_name=sys.argv[1]
+
+if file_name.endswith('.mha'):  # mha to mhd
+	
+	mhd_name = file_name[:-4]+'.mhd'
+	raw_name = file_name[:-4]+'.raw'
+	
+	mha_file = open(file_name, 'r')
+	mhd_file = open (mhd_name, 'w')
+	raw_file = open(raw_name, 'w')
+	
+	end_header = 0
+
+	for i, line in enumerate(mha_file):
+		if line.startswith('ElementDataFile ='):
+			end_header = i
+			break
+		if i > 40:
+			break
+	
+	mha_file.seek(0)
+	for k, line in enumerate(mha_file):
+		if k < end_header:
+			mhd_file.write(line)
+		elif k == end_header:
+			mhd_file.write('ElementDataFile = '+raw_name+'\n')
+		elif k > end_header:
+			raw_file.write(line)
+			
+	raw_file.close()
+	mhd_file.close()
+	mha_file.close()
+
+	
+elif file_name.endswith('.mhd'): # mhd to mha
+	
+	mha_name = file_name[:-4]+'.mha'
+	raw_name = file_name[:-4]+'.raw'
+	
+	header = open(file_name,'r')
+	mha_file = open(mha_name,'w')
+	
+	
+	for line in header:
+		if not line.startswith('ElementDataFile ='):
+			mha_file.write(line)
+		elif line.startswith('ElementDataFile ='):
+			mha_file.write('ElementDataFile = LOCAL\n')
+	
+	header.close()
+	
+	raw = open(raw_name, 'r')
+	r=raw.readlines()
+	mha_file.writelines(r)
+	
+	raw.close()
+	mha_file.close()
+	
+	
+elif not file_name.endswith('.mha') and not file_name.endswith('.mhd'):  # no mha/mhd file
+	print ("The input file isn't a mha/mhd file!")
---
+++ extra/python/mha.py
@@ -0,0 +1,102 @@
+## These functions read and write mha files (ONLY IMAGES)
+## Author: Paolo Zaffino  (p.zaffino@yahoo.it)
+## rev 9
+## NOT TESTED ON PYTHON 3
+
+import numpy as np
+
+def write (A, fn, spacing, offset, data_type):
+	
+	# A=3D matrix
+	# fn=filename
+	# data_type='short', 'float' or 'uchar'
+
+	f=open(fn, 'w')
+	f.write('ObjectType = Image\n')
+	f.write('NDims = 3\n')
+	f.write('BinaryData = True\n')
+	f.write('BinaryDataByteOrderMSB = False\n')
+	f.write('CompressedData = False\n')
+	f.write('TransformMatrix = 1 0 0 0 1 0 0 0 1\n')
+	off='Offset = '+str(offset[0])+' '+str(offset[1])+' '+str(offset[2])+'\n'
+	f.write(off)
+	f.write('CenterOfRotation = 0 0 0\n')
+	f.write('AnatomicalOrientation = RAI\n')
+	spac='ElementSpacing = '+str(spacing[0])+' '+str(spacing[1])+' '+str(spacing[2])+'\n'
+	f.write(spac)
+	dim='DimSize = '+str(A.shape[0])+' '+str(A.shape[1])+ ' '+str(A.shape[2])+'\n'
+	f.write(dim)
+	if data_type == 'short':
+		f.write('ElementType = MET_SHORT\n')
+	elif data_type == 'float':
+		f.write('ElementType = MET_FLOAT\n')
+	elif data_type == 'uchar':
+		f.write('ElementType = MET_UCHAR\n')
+	f.write('ElementDataFile = LOCAL\n')
+	
+	f.write(A)
+	
+	f.close()
+
+
+def read(fn):
+	
+	#fn=filename
+	
+	def cast2int (l):
+		for i in range(3):
+			if l[i].is_integer():
+				l[i]=int(l[i])
+		return l
+	
+	f = open(fn,'r')
+
+	for r in range(20):
+	
+		row=f.readline()
+	
+		if row.startswith('Offset ='):
+			row=row[9:]
+			offset=cast2int(map(float, row.split()))
+		elif row.startswith('ElementSpacing ='):
+			row=row[17:]
+			spacing=cast2int(map(float, row.split()))
+		elif row.startswith('DimSize ='):
+			row=row[10:]
+			siz=map(int, row.split())
+		elif row.startswith('ElementType ='):
+			data_type=row[14:-1]
+		elif row.startswith('ElementDataFile ='):
+			break
+	
+	img=f.readlines()
+	f.close()
+	
+	raw_img=''.join(img)
+	
+	if data_type == 'MET_SHORT':
+		raw=np.fromstring(raw_img, dtype=np.int16)
+		data_type = 'short'
+	elif data_type == 'MET_FLOAT':
+		raw=np.fromstring(raw_img, dtype=np.float32)
+		data_type = 'float'
+	elif data_type == 'MET_UCHAR':
+		raw=np.fromstring(raw_img, dtype=np.uint8)
+		data_type = 'uchar'
+	
+	raw=raw.reshape(siz[2],siz[1],siz[0])
+	raw=np.split(raw,1)
+	raw=raw[0]
+	
+	bidim_matr=[]
+	for i in range(siz[2]):
+	    bidim_matr.append(raw[i].transpose(np.roll(range(raw[i].ndim), -1)))
+	raw=np.dstack(bidim_matr)
+	
+	del bidim_matr
+	
+	return (raw, siz, spacing, offset, data_type)
+	
+	# raw=3D matrix
+	# siz=size
+	# data_type = 'short', 'float' or 'uchar'
