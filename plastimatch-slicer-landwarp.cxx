10b474d53d1abca49feb74be3a5a07da54130f20
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-04-19 20:12:47 +0000

    Fix possible bug with landwarp plugin

--- plastimatch-slicer-landwarp.cxx
+++ plastimatch-slicer-landwarp.cxx
@@ -15,9 +15,6 @@
 #include "math_util.h"
 #include "mha_io.h"
 #include "landmark_warp.h"
-//#include "landmark_warp_args.h"
-#include "landmark_warp_ggo.h"
-//#include "plm_ggo.h"
 #include "pointset.h"
 #include "print_and_exit.h"
 #include "rbf_gauss.h"
@@ -27,18 +24,6 @@
 // this .h is generated from ...landwarp.xml file by GenerateCLP in Slicer3-build
 #include "plastimatch-slicer-landwarpCLP.h"
 
-
-/* How do the algorithms load their point data (currently)?
-   plastimatch warp pointset   - PointSetType
-                                 itk_pointset.h
-   itk_tps_warp                - TPS_parms + PointSetType (equivalent)
-                                 itk_tps.h
-   GCS (landmark warp)         - Tps_xform (includes alpha, alpha * dist)
-                                 tps.h
-   NSH (bspline_rbf)           - Bspline_landmarks (includes warped lm, others)
-                                 bspline_landmarks.h
-*/
-
 static void
 do_landmark_warp_itk_tps (Landmark_warp *lw)
 {
@@ -47,7 +32,6 @@ do_landmark_warp_itk_tps (Landmark_warp *lw)
 
 static void
 do_landmark_warp_wendland (Landmark_warp *lw)  
-//formerly ..._warp_gcs, now with Wendland instead of cone. Note dead code rbf_gcs_warp
 {
     rbf_wendland_warp (lw);
 }
@@ -56,87 +40,6 @@ static void
 do_landmark_warp_gauss (Landmark_warp *lw)  //formerly do_landmark_warp_nsh
 {
    rbf_gauss_warp (lw);
-}
-
-
-
-static Landmark_warp*
-load_input_files (args_info_landmark_warp *args_info)
-{
-    Landmark_warp *lw = 0;
-
-//Load the input image 
-    lw->m_input_img = plm_image_load_native (args_info->input_image_arg);
-    if (!lw->m_input_img) 
-    {
-	print_and_exit ("Error reading moving file: %s\n", (const char*) args_info->input_image_arg);
-    }
-
-    /* Set the output geometry.  
-       Note: --offset, --spacing, and --dim get priority over --fixed. */
-    if (!args_info->origin_arg 
-	|| !args_info->spacing_arg 
-	|| !args_info->dim_arg) 
-    {
-	if (args_info->fixed_arg) {
-	    Plm_image *pli = plm_image_load_native (args_info->fixed_arg);
-	    if (!pli) {
-		print_and_exit ("Error loading fixed image: %s\n",
-		    args_info->fixed_arg);
-	    }
-	    lw->m_pih.set_from_plm_image (pli);
-	    delete pli;
-	} else {
-	    lw->m_pih.set_from_plm_image (lw->m_input_img);
-	}
-    }
-    if (args_info->origin_arg) {
-	int rc;
-	float f[3];
-	rc = sscanf (args_info->origin_arg, "%f %f %f", &f[0], &f[1], &f[2]);
-	if (rc != 3) {
-	    print_and_exit ("Error parsing origin: %s\n",
-		args_info->origin_arg);
-	}
-	lw->m_pih.set_origin (f);
-    }
-    if (args_info->spacing_arg) {
-	int rc;
-	float f[3];
-	rc = sscanf (args_info->spacing_arg, "%f %f %f", &f[0], &f[1], &f[2]);
-	if (rc != 3) {
-	    print_and_exit ("Error parsing spacing: %s\n",
-		args_info->spacing_arg);
-	}
-	lw->m_pih.set_spacing (f);
-    }
-    if (args_info->dim_arg) {
-	int rc;
-	int d[3];
-	rc = sscanf (args_info->dim_arg, "%d %d %d", &d[0], &d[1], &d[2]);
-	if (rc != 3) {
-	    print_and_exit ("Error parsing dim: %s\n",
-		args_info->dim_arg);
-	}
-	lw->m_pih.set_dim (d);
-    }
-
-    lw->rbf_radius = args_info->radius_arg;
-    lw->young_modulus = args_info->stiffness_arg;
-	lw->num_clusters = args_info->numclusters_arg;
-    return lw;
-}
-
-static void
-save_output_files (Landmark_warp *lw, args_info_landmark_warp *args_info)
-{
-    /* GCS FIX: float output only, and no dicom. */
-    if (lw->m_warped_img && args_info->output_image_arg) {
-	lw->m_warped_img->save_image (args_info->output_image_arg);
-    }
-    if (lw->m_vf && args_info->output_vf_arg) {
-	xform_save (lw->m_vf, args_info->output_vf_arg);
-    }
 }
 
 static void
@@ -157,13 +60,6 @@ do_landmark_warp (Landmark_warp *lw, const char *algorithm)
 */
 }
 
-static void
-check_arguments (args_info_landmark_warp *args_info)
-{
-    /* Nothing to check? */
-}
-
-
 int
 main (int argc, char *argv[])
 {	
@@ -172,17 +68,25 @@ main (int argc, char *argv[])
 
     Landmark_warp *lw = landmark_warp_create ();
 
+    /* Load moving image */
     lw->m_input_img = plm_image_load_native (
 	plmslc_landwarp_moving_volume.c_str());
-    Plm_image *tmp = plm_image_load_native (
-	plmslc_landwarp_fixed_volume.c_str());
-    lw->m_pih.set_from_plm_image (tmp);
+
+    /* Default geometry comes from moving image.
+       But if user specified fixed image, we use that. */
+    lw->m_pih.set_from_plm_image (lw->m_input_img);
+    if (plmslc_landwarp_fixed_volume != "") {
+	Plm_image *tmp = plm_image_load_native (
+	    plmslc_landwarp_fixed_volume.c_str());
+	lw->m_pih.set_from_plm_image (tmp);
+	delete tmp;
+    }
+
+    /* Set other parameters */
     lw->default_val=plmslc_landwarp_default_value;
     lw->rbf_radius=plmslc_landwarp_rbf_radius;
     lw->young_modulus=plmslc_landwarp_stiffness;
-	lw->num_clusters=plmslc_landwarp_num_clusters;
-
-    delete tmp;
+    lw->num_clusters=plmslc_landwarp_num_clusters;
 
     unsigned long num_fiducials = plmslc_landwarp_fixed_fiducials.size();
     if (plmslc_landwarp_moving_fiducials.size() < num_fiducials) {
@@ -200,10 +104,18 @@ main (int argc, char *argv[])
 
     for (unsigned long i = 0; i < num_fiducials; i++) {
 	
-	float lm_fix[3] = { plmslc_landwarp_fixed_fiducials[i][0],  plmslc_landwarp_fixed_fiducials[i][1],  plmslc_landwarp_fixed_fiducials[i][2]};
+	float lm_fix[3] = { 
+	    plmslc_landwarp_fixed_fiducials[i][0],  
+	    plmslc_landwarp_fixed_fiducials[i][1],  
+	    plmslc_landwarp_fixed_fiducials[i][2]
+	};
 	pointset_add_point (fix_ps, lm_fix);
     
-	float lm_mov[3] = { plmslc_landwarp_moving_fiducials[i][0],  plmslc_landwarp_moving_fiducials[i][1],  plmslc_landwarp_moving_fiducials[i][2]};
+	float lm_mov[3] = { 
+	    plmslc_landwarp_moving_fiducials[i][0],  
+	    plmslc_landwarp_moving_fiducials[i][1],  
+	    plmslc_landwarp_moving_fiducials[i][2]
+	};
 	pointset_add_point (mov_ps, lm_mov);
     }
 
