c0af84edbdce3eb7c0077b7eaf41ebafdb5c97ac
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-05-10 22:05:02 +0000

    Skeleton code for new image_header organization

---
+++ src/plastimatch/volume_header.cxx
@@ -0,0 +1,121 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "plm_config.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "itkImageRegionIterator.h"
+
+#include "mha_io.h"
+#include "plm_image.h"
+#include "print_and_exit.h"
+#include "volume.h"
+#include "volume_header.h"
+
+void
+Volume_header::set_dim (int dim[3])
+{
+    for (unsigned int d = 0; d < 3; d++) {
+	m_dim[d] = dim[d];
+    }
+}
+
+void
+Volume_header::set_origin (float origin[3])
+{
+    for (unsigned int d = 0; d < 3; d++) {
+	this->m_origin[d] = origin[d];
+    }
+}
+
+void
+Volume_header::set_spacing (float spacing[3])
+{
+    for (unsigned int d = 0; d < 3; d++) {
+	this->m_spacing[d] = spacing[d];
+    }
+}
+
+void
+Volume_header::set_direction_cosines (float direction_cosines[9])
+{
+    for (unsigned int d = 0; d < 9; d++) {
+	this->m_direction_cosines[d] = direction_cosines[d];
+    }
+}
+
+void
+Volume_header::set (
+    int dim[3],
+    float origin[3],
+    float spacing[3],
+    float direction_cosines[9])
+{
+    this->set_dim (dim);
+    this->set_origin (origin);
+    this->set_spacing (spacing);
+    this->set_direction_cosines (direction_cosines);
+}
+
+void
+Volume_header::set_from_bxf (Bspline_xform *bxf)
+{
+    this->set (
+	bxf->img_dim,
+	bxf->img_origin,
+	bxf->img_spacing,
+	0);
+}
+
+void
+Volume_header::print (void) const
+{
+    printf ("Dim =");
+    for (unsigned int d = 0; d < 3; d++) {
+	printf (" %d", m_dim[d]);
+    }
+    printf ("\nOrigin =");
+    for (unsigned int d = 0; d < 3; d++) {
+	printf (" %g", m_origin[d]);
+    }
+    printf ("\nSpacing =");
+    for (unsigned int d = 0; d < 3; d++) {
+	printf (" %g", m_spacing[d]);
+    }
+    printf ("\nDirection =");
+    for (unsigned int d1 = 0; d1 < 3; d1++) {
+	for (unsigned int d2 = 0; d2 < 3; d2++) {
+	    printf (" %g", m_direction_cosines[d1*3+d2]);
+	}
+    }
+    printf ("\n");
+}
+
+void 
+Volume_header::get_image_center (float center[3])
+{
+    int d;
+    for (d = 0; d < 3; d++) {
+	center[d] = this->m_origin[d] 
+	    + this->m_spacing[d] * (this->m_dim[d] - 1) / 2;
+    }
+}
+
+/* Return 1 if the two headers are the same */
+int
+Volume_header::compare (Volume_header *pli1, Volume_header *pli2)
+{
+    int d;
+    for (d = 0; d < 3; d++) {
+	if (pli1->m_dim[d] != pli2->m_dim[d]) return 0;
+	if (pli1->m_origin[d] != pli2->m_origin[d]) return 0;
+	if (pli1->m_spacing[d] != pli2->m_spacing[d]) return 0;
+    }
+    for (d = 0; d < 9; d++) {
+	if (pli1->m_direction_cosines[d] != pli2->m_direction_cosines[d]) {
+	    return 0;
+	}
+    }
+    return 1;
+}
---
+++ src/plastimatch/volume_header.h
@@ -0,0 +1,58 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#ifndef _volume_header_h_
+#define _volume_header_h_
+
+#include "plm_config.h"
+#include "xform.h"
+#include "volume.h"
+#include "itk_image.h"
+#include "print_and_exit.h"
+
+class Volume_header;
+class Plm_image;
+
+class plastimatch1_EXPORT Volume_header {
+public:
+    float m_origin[3];
+    float m_spacing[3];
+    int m_dim[3];
+    float m_direction_cosines[9];
+
+public:
+    Volume_header () {}
+    Volume_header (int dim[3], float origin[3], float spacing[3])
+    {
+	this->set (dim, origin, spacing, 0);
+    }
+    Volume_header (int dim[3], float origin[3], float spacing[3],
+	float direction_cosines[9])
+    {
+	this->set (dim, origin, spacing, direction_cosines);
+    }
+
+public:
+    /* Return 1 if the two headers are the same */
+    static int compare (Volume_header *pli1, Volume_header *pli2);
+
+public:
+    void set_dim (int dim[3]);
+    void set_origin (float origin[3]);
+    void set_spacing (float spacing[3]);
+    void set_direction_cosines (float direction_cosines[9]);
+
+    void set (int dim[3], float origin[3], float spacing[3],
+	float direction_cosines[9]);
+    void set_from_bxf (Bspline_xform *bxf);
+
+    static void clone (Volume_header *dest, Volume_header *src) {
+	dest->set (src->m_dim, src->m_origin, src->m_spacing, 
+	    src->m_direction_cosines);
+    }
+
+    void print (void) const;
+    void get_image_center (float center[3]);
+};
+
+#endif
