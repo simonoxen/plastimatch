e6a4f00cc3d4c68fcee6539cd2ad8da38c71dcb3
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-05-13 22:46:00 +0000

    More lowercasing of cmake files

--- cmake/FindCUDA_wrap.cmake
+++ cmake/FindCUDA_wrap.cmake
@@ -19,35 +19,35 @@ else ()
   find_package (CUDA QUIET)
 endif ()
 
-SET (CUDA_FOUND ${CUDA_FOUND} CACHE BOOL "Did we find cuda?")
+set (CUDA_FOUND ${CUDA_FOUND} CACHE BOOL "Did we find cuda?")
 
-IF (CUDA_FOUND)
-  CUDA_INCLUDE_DIRECTORIES (
+if (CUDA_FOUND)
+  cuda_include_directories (
     ${CMAKE_CURRENT_SOURCE_DIR}
     )
-ENDIF()
+endif ()
 
 # JAS 08.25.2010
 #   Check to make sure nvcc has gcc-4.3 for compiling.
 #   This script will modify CUDA_NVCC_FLAGS if system default is not gcc-4.3
-INCLUDE (nvcc-check)
+include (nvcc-check)
 
 # JAS 2010.12.09
 #   Build code for all known compute capabilities by default.
 #   When developing, it is sometimes nice to turn this off in order
 #   to speed up the build processes (since you only have 1 GPU in your machine).
-SET (PLM_CUDA_ALL_DEVICES ON CACHE BOOL 
+set (PLM_CUDA_ALL_DEVICES ON CACHE BOOL 
   "Generate GPU code for all compute capabilities?")
-IF (PLM_CUDA_ALL_DEVICES)
-  MESSAGE (STATUS "CUDA Build Level: ALL Compute Capabilities")
-  SET (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}
+if (PLM_CUDA_ALL_DEVICES)
+  message (STATUS "CUDA Build Level: ALL Compute Capabilities")
+  set (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}
         -gencode arch=compute_10,code=sm_10
         -gencode arch=compute_11,code=sm_11
         -gencode arch=compute_12,code=sm_12
         -gencode arch=compute_13,code=sm_13
         -gencode arch=compute_20,code=sm_20
     )
-ELSE ()
-  MESSAGE (STATUS "CUDA Build Level: Build system Compute Capability ONLY!")
-ENDIF ()
+else ()
+  message (STATUS "CUDA Build Level: Build system Compute Capability ONLY!")
+endif ()
 
