48b60cb10eccca8f06b8ef1ac7269ed29e73419e
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-06-14 19:37:28 +0000

    Work on numeric second derivative computations

---
+++ extra/perl/synthetic_bspline_coefficients.pl
@@ -0,0 +1,27 @@
+$header = <<EODATA
+MGH_GPUIT_BSP <experimental>
+img_origin = -187.500000 -187.500000 -187.500000
+img_spacing = .01 .01 .01
+img_dim = 100 100 100
+roi_offset = 0 0 0
+roi_dim = 100 100 100
+vox_per_rgn = 100 100 100
+EODATA
+  ;
+print $header;
+
+for $xyz (0..2) {
+for $z (1..4) {
+for $y (1..4) {
+for $x (0..3) {
+    if ($xyz == 0) {
+	print $x * $x, "\n";
+    } elsif ($xyz == 1) {
+	print "1\n";
+    } else {
+	print "1\n";
+    }
+}
+}
+}
+}
--- src/plastimatch/reg.cxx
+++ src/plastimatch/reg.cxx
@@ -106,9 +106,9 @@ vf_regularize_numerical (Volume* vol)
     int idx_kn, idx_kp;
 
     /* Indicies of POI's diagonal neighbors */
-    int idx_ijn, idx_ijp;
-    int idx_ikn, idx_ikp;
-    int idx_jkn, idx_jkp;
+    int idx_injn, idx_injp, idx_ipjn, idx_ipjp;
+    int idx_inkn, idx_inkp, idx_ipkn, idx_ipkp;
+    int idx_jnkn, idx_jnkp, idx_jpkn, idx_jpkp;
 
     /* Deformation vector @ POI */
     float *vec_poi;
@@ -119,9 +119,12 @@ vf_regularize_numerical (Volume* vol)
     float *vec_kn, *vec_kp;
 
     /* Vectors of POI's diagonal neighbors */
-    float *vec_ijn, *vec_ijp;
-    float *vec_ikn, *vec_ikp;
-    float *vec_jkn, *vec_jkp;
+    float *vec_injn, *vec_injp;
+    float *vec_ipjn, *vec_ipjp;
+    float *vec_inkn, *vec_inkp;
+    float *vec_ipkn, *vec_ipkp;
+    float *vec_jnkn, *vec_jnkp;
+    float *vec_jpkn, *vec_jpkp;
 
     /* Vector's partial spatial derivatives */
     float d2_dx2[3],  d2_dxdy[3];
@@ -156,12 +159,18 @@ vf_regularize_numerical (Volume* vol)
                 idx_kn = INDEX_OF (vol->dim,   i,   j, k-1);
                 idx_kp = INDEX_OF (vol->dim,   i,   j, k+1);
 
-                idx_ijn = INDEX_OF (vol->dim, i-1, j-1,   k);
-                idx_ijp = INDEX_OF (vol->dim, i+1, j+1,   k);
-                idx_ikn = INDEX_OF (vol->dim, i-1,   j, k-1);
-                idx_ikp = INDEX_OF (vol->dim, i+1,   j, k+1);
-                idx_jkn = INDEX_OF (vol->dim,   i, j-1, k-1);
-                idx_jkp = INDEX_OF (vol->dim,   i, j+1, k+1);
+                idx_injn = INDEX_OF (vol->dim, i-1, j-1,   k);
+                idx_injp = INDEX_OF (vol->dim, i-1, j+1,   k);
+                idx_ipjn = INDEX_OF (vol->dim, i+1, j-1,   k);
+                idx_ipjp = INDEX_OF (vol->dim, i+1, j+1,   k);
+                idx_inkn = INDEX_OF (vol->dim, i-1,   j, k-1);
+                idx_inkp = INDEX_OF (vol->dim, i-1,   j, k+1);
+                idx_ipkn = INDEX_OF (vol->dim, i+1,   j, k-1);
+                idx_ipkp = INDEX_OF (vol->dim, i+1,   j, k+1);
+                idx_jnkn = INDEX_OF (vol->dim,   i, j-1, k-1);
+                idx_jnkp = INDEX_OF (vol->dim,   i, j-1, k+1);
+                idx_jpkn = INDEX_OF (vol->dim,   i, j+1, k-1);
+                idx_jpkp = INDEX_OF (vol->dim,   i, j+1, k+1);
 
                 /* Load vectors relevant to current POI */
                 vec_poi = &img[3*idx_poi];
@@ -170,9 +179,12 @@ vf_regularize_numerical (Volume* vol)
                 vec_jn = &img[3*idx_jn]; vec_jp = &img[3*idx_jp];
                 vec_kn = &img[3*idx_kn]; vec_kp = &img[3*idx_kp];
 
-                vec_ijn = &img[3*idx_ijn]; vec_ijp = &img[3*idx_ijp];
-                vec_ikn = &img[3*idx_ikn]; vec_ikp = &img[3*idx_ikp];
-                vec_jkn = &img[3*idx_jkn]; vec_jkp = &img[3*idx_jkp];
+                vec_injn = &img[3*idx_injn]; vec_injp = &img[3*idx_injp];
+                vec_ipjn = &img[3*idx_ipjn]; vec_ipjp = &img[3*idx_ipjp];
+                vec_inkn = &img[3*idx_inkn]; vec_inkp = &img[3*idx_inkp];
+                vec_ipkn = &img[3*idx_ipkn]; vec_ipkp = &img[3*idx_ipkp];
+                vec_jnkn = &img[3*idx_jnkn]; vec_jnkp = &img[3*idx_jnkp];
+                vec_jpkn = &img[3*idx_jpkn]; vec_jpkp = &img[3*idx_jpkp];
 
                 /* Compute components */
                 d2_sq = 0.0f;
@@ -181,6 +193,14 @@ vf_regularize_numerical (Volume* vol)
                     d2_dy2[c] = inv_dydy * (vec_jp[c] - 2.0f*vec_poi[c] + vec_jn[c]);
                     d2_dz2[c] = inv_dzdz * (vec_kp[c] - 2.0f*vec_poi[c] + vec_kn[c]);
 
+                    d2_dxdy[c] = inv_dxdy * (
+			vec_injn[c] - vec_injp[c] - vec_ipjn[c] + vec_ipjp[c]);
+                    d2_dxdz[c] = inv_dxdz * (
+			vec_inkn[c] - vec_inkp[c] - vec_ipkn[c] + vec_ipkp[c]);
+                    d2_dydz[c] = inv_dydz * (
+			vec_jnkn[c] - vec_jnkp[c] - vec_jpkn[c] + vec_jpkp[c]);
+
+#if defined (commentout)
                     d2_dxdy[c] = inv_dxdy * (
                             (vec_ijp[c] + 2.0f*vec_poi[c] + vec_ijn[c]) +
                             (vec_ip[c] + vec_in[c] + vec_jp[c] + vec_jn[c])
@@ -193,7 +213,7 @@ vf_regularize_numerical (Volume* vol)
                             (vec_jkp[c] + 2.0f*vec_poi[c] + vec_jkn[c]) +
                             (vec_jp[c] + vec_jn[c] + vec_kp[c] + vec_kn[c])
                         );
-
+#endif
                     d2_sq += d2_dx2[c]*d2_dx2[c] + d2_dy2[c]*d2_dy2[c] +
                              d2_dz2[c]*d2_dz2[c] + 2.0f * (
                                 d2_dxdy[c]*d2_dxdy[c] +
