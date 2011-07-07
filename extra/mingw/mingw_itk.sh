cfd7cf936bde59422d7111431b3f2e549b769f55
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-07-07 13:23:13 +0000

    Improve MinGW scripts

--- extra/mingw/mingw_itk.sh
+++ extra/mingw/mingw_itk.sh
@@ -1,4 +1,13 @@
-/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake \
+if test -f /c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake; then
+    CMAKE_EXE=/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake
+elif test -f /c/Program\ Files/CMake\ 2.8/bin/cmake; then
+    CMAKE_EXE=/c/Program\ Files/CMake\ 2.8/bin/cmake
+else
+    echo "CMake not found!"
+    return 1
+fi
+
+"${CMAKE_EXE}" \
 	-G"MSYS Makefiles" \
 	-DBUILD_EXAMPLES=OFF \
 	-DBUILD_TESTING=OFF \
--- extra/mingw/mingw_plastimatch.sh
+++ extra/mingw/mingw_plastimatch.sh
@@ -1,6 +1,18 @@
-/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake \
+if test -f /c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake; then
+    CMAKE_EXE=/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/cmake
+elif test -f /c/Program\ Files/CMake\ 2.8/bin/cmake; then
+    CMAKE_EXE=/c/Program\ Files/CMake\ 2.8/bin/cmake
+else
+    echo "CMake not found!"
+    return 1
+fi
+
+#ITK_DIR=/c/gcs6/build/itk-3.20.0-mingw
+ITK_DIR=/c/gcs6/build/mingw/itk-3.20.0
+
+"${CMAKE_EXE}" \
 	-G"MSYS Makefiles" \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DITK_DIR=/c/gcs6/build/itk-3.20.0-mingw \
+	-DITK_DIR="${ITK_DIR}" \
 	/c/gcs6/work/plastimatch
 
