69277f58bf31a541e97e5adac644da928bb3f957
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-07-15 23:14:26 +0000

    Add another cdash machine

---
+++ extra/cdash/physics.cmake
@@ -0,0 +1,22 @@
+SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/work/plastimatch")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/build/nightly/plastimatch")
+SET (CTEST_CMAKE_COMMAND "/usr/local/bin/cmake")
+SET (CTEST_COMMAND "/usr/local/bin/ctest -D Nightly")
+SET (CTEST_INITIAL_CACHE "
+//Name of generator.
+CMAKE_GENERATOR:INTERNAL=Unix Makefiles
+
+//Name of the build
+BUILDNAME:STRING=linux-64
+
+//Name of the computer/site where compile is being run
+SITE:STRING=physics
+
+//The directory containing ITKConfig.cmake.  This is either the
+// root of the build tree, or PREFIX/lib/InsightToolkit for an
+// installation.
+ITK_DIR:PATH=/home/gcs6/build/itk-3.18.0
+
+//Build with shared libraries.
+BUILD_SHARED_LIBS:BOOL=ON
+")
