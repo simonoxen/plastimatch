9e894f5885ff18b6e163b637373e95ed6a8cf45b
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-07-26 21:38:37 +0000

    Fix build problem for smithwick dashboard

--- extra/cdash/smithwick.cmake
+++ extra/cdash/smithwick.cmake
@@ -12,11 +12,6 @@ BUILDNAME:STRING=linux-32
 //Name of the computer/site where compile is being run
 SITE:STRING=smithwick
 
-//The directory containing ITKConfig.cmake.  This is either the
-// root of the build tree, or PREFIX/lib/InsightToolkit for an
-// installation.
-ITK_DIR:PATH=/home/gcs6/build/itk-3.20.0
-
 //Build with shared libraries.
 BUILD_SHARED_LIBS:BOOL=ON
 ")
