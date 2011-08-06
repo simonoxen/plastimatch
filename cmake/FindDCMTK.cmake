af3c6476a9ddfabfb2661636cfbc5a4b182c09ea
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-08-06 16:24:54 +0000

    Decapitalize FindDCMTK

--- cmake/FindDCMTK.cmake
+++ cmake/FindDCMTK.cmake
@@ -22,53 +22,53 @@
 #   libtiff4-dev
 #   libwrap0-dev
 
-INCLUDE(CheckLibraryExists)
-INCLUDE(FindThreads)
+include(CheckLibraryExists)
+include(FindThreads)
 
-FIND_PACKAGE (ZLIB)
-IF (ZLIB_FOUND)
-  MESSAGE (STATUS "Looking for ZLIB - found")
-ELSE (ZLIB_FOUND)
-  MESSAGE (STATUS "Looking for ZLIB - not found")
-ENDIF (ZLIB_FOUND)
+find_package (ZLIB)
+if (ZLIB_FOUND)
+  message (STATUS "Looking for ZLIB - found")
+else ()
+  message (STATUS "Looking for ZLIB - not found")
+endif ()
 
-FIND_PACKAGE (PNG)
-IF (PNG_FOUND)
-  MESSAGE (STATUS "Looking for PNG - found")
-ELSE (PNG_FOUND)
-  MESSAGE (STATUS "Looking for PNG - not found")
-ENDIF (PNG_FOUND)
+find_package (PNG)
+if (PNG_FOUND)
+  message (STATUS "Looking for PNG - found")
+else ()
+  message (STATUS "Looking for PNG - not found")
+endif ()
 
-FIND_PACKAGE (TIFF)
-IF (TIFF_FOUND)
-  MESSAGE (STATUS "Looking for TIFF - found")
-ELSE (TIFF_FOUND)
-  MESSAGE (STATUS "Looking for TIFF - not found")
-ENDIF (TIFF_FOUND)
+find_package (TIFF)
+if (TIFF_FOUND)
+  message (STATUS "Looking for TIFF - found")
+else ()
+  message (STATUS "Looking for TIFF - not found")
+endif ()
 
-FIND_LIBRARY (SSL_LIBRARY ssl)
-IF (SSL_LIBRARY)
-  MESSAGE (STATUS "Looking for SSL - found: ${SSL_LIBRARY}")
-ELSE (SSL_LIBRARY)
-  MESSAGE (STATUS "Looking for SSL - not found")
-ENDIF (SSL_LIBRARY)
+find_library (SSL_LIBRARY ssl)
+if (SSL_LIBRARY)
+  message (STATUS "Looking for SSL - found: ${SSL_LIBRARY}")
+else ()
+  message (STATUS "Looking for SSL - not found")
+endif ()
 
-FIND_PATH (
+find_path (
   DCMTK_INCLUDE_DIR dcmtk/config/osconfig.h
   ${DCMTK_DIR}/include
   /usr/local/dicom/include
   )
 
-IF (UNIX)
-  FIND_FILE (DCMTK_HAVE_CONFIG_H dcmtk/config/cfunix.h
+if (UNIX)
+  find_file (DCMTK_HAVE_CONFIG_H dcmtk/config/cfunix.h
     ${DCMTK_DIR}/include
     /usr/local/dicom/include
     )
-ELSE ()
-  SET (DCMTK_HAVE_CONFIG_H FALSE)
-ENDIF ()
+else ()
+  set (DCMTK_HAVE_CONFIG_H FALSE)
+endif ()
 
-FIND_LIBRARY( DCMTK_dcmimgle_LIBRARY dcmimgle
+find_library (DCMTK_dcmimgle_LIBRARY dcmimgle
   ${DCMTK_DIR}/dcmimgle/libsrc
   ${DCMTK_DIR}/dcmimgle/libsrc/Release
   ${DCMTK_DIR}/dcmimgle/libsrc/Debug
@@ -79,7 +79,7 @@ FIND_LIBRARY( DCMTK_dcmimgle_LIBRARY dcmimgle
 )
 
 # This is gone in 3.6
-FIND_LIBRARY(DCMTK_imagedb_LIBRARY 
+find_library (DCMTK_imagedb_LIBRARY 
   NAMES imagedb dcmimage
   PATHS
   ${DCMTK_DIR}/imagectn/libsrc/Release
@@ -89,7 +89,7 @@ FIND_LIBRARY(DCMTK_imagedb_LIBRARY
   /usr/local/dicom/lib
 )
 
-FIND_LIBRARY (DCMTK_dcmtls_LIBRARY dcmtls 
+find_library (DCMTK_dcmtls_LIBRARY dcmtls 
   ${DCMTK_DIR}/dcmnet/libsrc/Release
   ${DCMTK_DIR}/dcmnet/libsrc/Debug
   ${DCMTK_DIR}/dcmnet/libsrc
@@ -100,7 +100,7 @@ FIND_LIBRARY (DCMTK_dcmtls_LIBRARY dcmtls
   /usr/local/dicom/lib
 )
 
-FIND_LIBRARY (DCMTK_dcmnet_LIBRARY dcmnet 
+find_library (DCMTK_dcmnet_LIBRARY dcmnet 
   ${DCMTK_DIR}/dcmnet/libsrc/Release
   ${DCMTK_DIR}/dcmnet/libsrc/Debug
   ${DCMTK_DIR}/dcmnet/libsrc/
@@ -108,7 +108,7 @@ FIND_LIBRARY (DCMTK_dcmnet_LIBRARY dcmnet
   /usr/local/dicom/lib
 )
 
-FIND_LIBRARY( DCMTK_dcmdata_LIBRARY dcmdata
+find_library (DCMTK_dcmdata_LIBRARY dcmdata
   ${DCMTK_DIR}/dcmdata/libsrc
   ${DCMTK_DIR}/dcmdata/libsrc/Release
   ${DCMTK_DIR}/dcmdata/libsrc/Debug
@@ -119,12 +119,12 @@ FIND_LIBRARY( DCMTK_dcmdata_LIBRARY dcmdata
 )
 
 # Quick hack: dcmtk 3.6.0
-FIND_LIBRARY(DCMTK_oflog_LIBRARY oflog
+find_library (DCMTK_oflog_LIBRARY oflog
   ${DCMTK_DIR}/lib
   /usr/local/dicom/lib
 )
 
-FIND_LIBRARY(DCMTK_ofstd_LIBRARY ofstd
+find_library (DCMTK_ofstd_LIBRARY ofstd
   ${DCMTK_DIR}/ofstd/libsrc
   ${DCMTK_DIR}/ofstd/libsrc/Release
   ${DCMTK_DIR}/ofstd/libsrc/Debug
@@ -134,17 +134,17 @@ FIND_LIBRARY(DCMTK_ofstd_LIBRARY ofstd
   /usr/local/dicom/lib
 )
 
-IF (DCMTK_INCLUDE_DIR 
+if (DCMTK_INCLUDE_DIR 
     AND DCMTK_dcmnet_LIBRARY
     AND DCMTK_ofstd_LIBRARY
     AND DCMTK_dcmdata_LIBRARY
     AND DCMTK_dcmimgle_LIBRARY)
 
-  SET (DCMTK_FOUND "YES")
+  set (DCMTK_FOUND "YES")
 
-  SET (DCMTK_LIBRARIES "")
+  set (DCMTK_LIBRARIES "")
 
-  SET (DCMTK_LIBRARIES 
+  set (DCMTK_LIBRARIES 
     ${DCMTK_LIBRARIES}
     ${DCMTK_dcmtls_LIBRARY}
     ${DCMTK_dcmnet_LIBRARY}
@@ -152,110 +152,109 @@ IF (DCMTK_INCLUDE_DIR
     ${DCMTK_dcmdata_LIBRARY}
     )
 
-  IF (DCMTK_oflog_LIBRARY)
-    SET (DCMTK_LIBRARIES
+  if (DCMTK_oflog_LIBRARY)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${DCMTK_oflog_LIBRARY}
       )
-  ENDIF ()
+  endif ()
 
-  SET (DCMTK_LIBRARIES 
+  set (DCMTK_LIBRARIES 
     ${DCMTK_LIBRARIES}
     ${DCMTK_ofstd_LIBRARY}
     )
 
-  IF (SSL_LIBRARY)
-    SET (DCMTK_LIBRARIES
+  if (SSL_LIBRARY)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${SSL_LIBRARY}
       )
-  ENDIF ()
+  endif ()
 
-  IF (PNG_FOUND)
-    SET (DCMTK_LIBRARIES
+  if (PNG_FOUND)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${PNG_LIBRARIES})
-  ENDIF ()
+  endif ()
 
-  IF (TIFF_FOUND)
-    SET (DCMTK_LIBRARIES
+  if (TIFF_FOUND)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${TIFF_LIBRARIES})
-  ENDIF ()
+  endif ()
 
-  IF (ZLIB_FOUND)
-    SET (DCMTK_LIBRARIES
+  if (ZLIB_FOUND)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${ZLIB_LIBRARIES})
-  ENDIF ()
+  endif ()
 
-  IF (CMAKE_THREAD_LIBS_INIT)
-    SET (DCMTK_LIBRARIES
+  if (CMAKE_THREAD_LIBS_INIT)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${CMAKE_THREAD_LIBS_INIT})
-  ENDIF ()
+  endif ()
 
-  FIND_LIBRARY (LIBWRAP_LIBRARY NAMES wrap libwrap PATHS /lib)
-  IF (LIBWRAP_LIBRARY)
-    SET (DCMTK_LIBRARIES
+  find_library (LIBWRAP_LIBRARY NAMES wrap libwrap PATHS /lib)
+  if (LIBWRAP_LIBRARY)
+    set (DCMTK_LIBRARIES
       ${DCMTK_LIBRARIES}
       ${LIBWRAP_LIBRARY})
-  ENDIF ()
+  endif ()
 
-  IF (WIN32)
-    SET(DCMTK_LIBRARIES ${DCMTK_LIBRARIES} netapi32 ws2_32)
-  ENDIF ()
+  if (WIN32)
+    set (DCMTK_LIBRARIES ${DCMTK_LIBRARIES} netapi32 ws2_32)
+  endif ()
+endif ()
 
-ENDIF ()
+if (NOT DCMTK_FOUND)
+  set (DCMTK_DIR "" CACHE PATH "Root of DCMTK source tree (optional).")
+endif ()
 
-IF (NOT DCMTK_FOUND)
-  SET (DCMTK_DIR "" CACHE PATH "Root of DCMTK source tree (optional).")
-ENDIF ()
-
-IF (DCMTK_FOUND)
-    FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/dcmdata/dcuid.h" 
+if (DCMTK_FOUND)
+    file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/dcmdata/dcuid.h" 
       DCMTK_VERSION_STRING
       REGEX "^#define OFFIS_DCMTK_VERSION_STRING *\"([^\"]*)\"")
-    IF (NOT DCMTK_VERSION_STRING)
-      FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/osconfig.h"
+    if (NOT DCMTK_VERSION_STRING)
+      file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/osconfig.h"
         DCMTK_VERSION_STRING
         REGEX "^#define PACKAGE_VERSION *\"([^\"]*)\"")
-    ENDIF ()
-    IF (NOT DCMTK_VERSION_STRING)
-      FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/cfunix.h"
+    endif ()
+    if (NOT DCMTK_VERSION_STRING)
+      file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/cfunix.h"
         DCMTK_VERSION_STRING
         REGEX "^#define PACKAGE_VERSION *\"([^\"]*)\"")
-    ENDIF ()
-    IF (DCMTK_VERSION_STRING)
+    endif ()
+    if (DCMTK_VERSION_STRING)
        # GCS: The below doesn't seem to work on Mac CMake 2.6.4.
        #  SET (DCMTK_VERSION_STRING "${CMAKE_MATCH_1}")
-       STRING (REGEX REPLACE "[^\"]*\"([^\"]*)\".*" "\\1"
+       string (REGEX REPLACE "[^\"]*\"([^\"]*)\".*" "\\1"
          DCMTK_VERSION_STRING "${DCMTK_VERSION_STRING}")
-    ENDIF ()
+    endif ()
 
-    FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/dcmdata/dcuid.h" 
+    file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/dcmdata/dcuid.h" 
       DCMTK_VERSION_NUMBER
       REGEX "^#define OFFIS_DCMTK_VERSION_NUMBER *([0-9]+)")
-    IF (NOT DCMTK_VERSION_NUMBER)
-      FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/osconfig.h"
+    if (NOT DCMTK_VERSION_NUMBER)
+      file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/osconfig.h"
         DCMTK_VERSION_NUMBER
         REGEX "^#define PACKAGE_VERSION_NUMBER *\"([0-9]+)\"")
-    ENDIF ()
-    IF (NOT DCMTK_VERSION_NUMBER)
-      FILE (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/cfunix.h"
+    endif ()
+    if (NOT DCMTK_VERSION_NUMBER)
+      file (STRINGS "${DCMTK_INCLUDE_DIR}/dcmtk/config/cfunix.h"
         DCMTK_VERSION_NUMBER
         REGEX "^#define PACKAGE_VERSION_NUMBER *\"([0-9]+)\"")
-    ENDIF ()
-    IF (DCMTK_VERSION_NUMBER)
-      STRING (REGEX REPLACE "[^0-9]*([0-9]+).*" "\\1"
+    endif ()
+    if (DCMTK_VERSION_NUMBER)
+      string (REGEX REPLACE "[^0-9]*([0-9]+).*" "\\1"
         DCMTK_VERSION_NUMBER "${DCMTK_VERSION_NUMBER}")
-    ENDIF ()
+    endif ()
 
-    MESSAGE (STATUS "DCMTK version is ${DCMTK_VERSION_STRING}")
-ENDIF ()
+    message (STATUS "DCMTK version is ${DCMTK_VERSION_STRING}")
+endif ()
 
-IF (DCMTK_FOUND)
-  MESSAGE (STATUS "Looking for dcmtk - found.")
-ELSE ()
-  MESSAGE (STATUS "Looking for dcmtk - not found.")
-ENDIF ()
+if (DCMTK_FOUND)
+  message (STATUS "Looking for dcmtk - found.")
+else ()
+  message (STATUS "Looking for dcmtk - not found.")
+endif ()
