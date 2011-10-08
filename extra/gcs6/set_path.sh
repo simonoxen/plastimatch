b2f9fa831eb02e5e9f842663a09e70fba5cebbd9
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2011-10-08 17:37:04 +0000

    Update paths

--- extra/gcs6/set_path.sh
+++ extra/gcs6/set_path.sh
@@ -2,23 +2,27 @@
 
 CNAME=`uname -n`
 case $CNAME in
-    "slumber")
-	export PATH=$PATH:/cygdrive/c/gcs6/build/plastimatch-cygwin
-	;;
-    "physics.mgh.harvard.edu")
-	export PATH=$PATH:$HOME/build/plastimatch-3.18.0
+    "absinthe")
+	export PATH=$PATH:$HOME/build/plastimatch-3.20.0
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
     "gelato")
 	export PATH=$PATH:$HOME/build/plastimatch-3.20.0
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
-    "wormwood")
-	export PATH=$PATH:$HOME/build/plastimatch-3.20.0
+    "physics.mgh.harvard.edu")
+	export PATH=$PATH:$HOME/build/plastimatch-3.18.0
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
     "redfish")
 	export PATH=$PATH:$HOME/build/plastimatch-3.18.0
+	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
+	;;
+    "slumber")
+	export PATH=$PATH:/cygdrive/c/gcs6/build/plastimatch-cygwin
+	;;
+    "wormwood")
+	export PATH=$PATH:$HOME/build/plastimatch-3.20.0
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
     *)
