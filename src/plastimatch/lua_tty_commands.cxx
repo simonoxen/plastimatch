523358d56a2e76ec6f9d790682ad5418d51ddd19
James A Shackleford <shack@linux.com>
2012-03-06 04:43:38 +0000

    Fancy new help system for interactive scripting mode

--- src/plastimatch/lua_tty_commands.cxx
+++ src/plastimatch/lua_tty_commands.cxx
@@ -20,6 +20,45 @@ extern "C" {
 
 
 static void
+print_command_table (
+    const char* cmds[], /* command array   */
+    unsigned int nc,    /* # of commands   */
+    unsigned int tw,    /* table width     */
+    unsigned int sp     /* minimum spacing */
+)
+{
+    unsigned int i;
+    unsigned int c,w,n;     /* col, word, command # */
+    unsigned int lc;        /* longest command      */
+
+    lc = 0;
+    for (i=0; i<nc; i++) {
+        if (strlen (cmds[i]) > lc) {
+            lc = strlen (cmds[i]);
+        }
+    }
+
+    c=0;w=0;n=0;
+    while (n<nc) {
+        while (c<tw) {
+            while (w<strlen(cmds[n])) {
+                fprintf (stdout, "%c", cmds[n][w++]);
+                c++;
+            }
+            w=0;
+            while (w<(lc-strlen(cmds[n])+sp)) {
+                fprintf (stdout, " ");
+                w++;c++;
+            }
+            n++; w=0;
+            if (n>=nc) break;
+        }
+        printf ("\n"); c=0;
+    }
+
+}
+
+static void
 build_args (int* argc, char*** argv, char* cmd)
 {
     int i;
@@ -48,7 +87,7 @@ build_args (int* argc, char*** argv, char* cmd)
 }
 
 static void
-do_tty_command_help (lua_State* L, int argc, char** argv)
+do_tty_command_run (lua_State* L, int argc, char** argv)
 {
     /* run lua script */
     if (argc < 2) return;
@@ -58,6 +97,31 @@ do_tty_command_help (lua_State* L, int argc, char** argv)
     } else {
         printf ("unable to load script: %s\n", argv[1]);
     }
+}
+
+
+static void
+do_tty_command_help (lua_State* L, int argc, char** argv)
+{
+    if (argc == 1) {
+        print_command_table (tty_cmds, num_tty_cmds, 75, 3);
+    } else {
+        if (!strcmp (argv[1], TTY_CMD_PCMD)) {
+            print_command_table (pcmds, num_pcmds, 75, 3);
+        }
+        else if (!strcmp (argv[1], TTY_CMD_RUN)) {
+            fprintf (stdout, "execute a script from disk\n");
+            fprintf (stdout, "  Usage: " TTY_CMD_RUN " script_name\n");
+        }
+        else if (!strcmp (argv[1], TTY_CMD_DIR)) {
+            fprintf (stdout, "get listing for current directory\n");
+        }
+        else if (!strcmp (argv[1], TTY_CMD_LS)) {
+            fprintf (stdout, "get listing for current directory\n");
+        }
+    }
+
+    fprintf (stdout, "\n");
 }
 
 void
@@ -73,8 +137,11 @@ do_tty_command (lua_State *L)
 
     build_args (&argc, &argv, cmd);
 
-    if (!strcmp (argv[0], TTY_CMD_RUN)) {
+    if (!strcmp (argv[0], TTY_CMD_HELP)) {
         do_tty_command_help (L, argc, argv);
+    }
+    else if (!strcmp (argv[0], TTY_CMD_RUN)) {
+        do_tty_command_run (L, argc, argv);
     }
     else if (!strcmp (argv[0], TTY_CMD_DIR) ||
              !strcmp (argv[0], TTY_CMD_LS)) {
