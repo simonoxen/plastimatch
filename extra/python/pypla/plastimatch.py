26d057720fe4eff17db7e80f3461c23e05cb8cfc
Paolo Zaffino <p.zaffino@yahoo.it>
2012-03-14 16:26:10 +0000

    [Pypla] Add four new features: add, diff, fill, mask and resample

--- extra/python/pypla/plastimatch.py
+++ extra/python/pypla/plastimatch.py
@@ -1,60 +1,57 @@
 ########################################################################
 ## This is a Python wrapper for Plastimatch
 ## Author: Paolo Zaffino  (p.zaffino@yahoo.it)
-## Rev 1
+## Rev 2
 ## NOT TESTED ON PYTHON 3
 ########################################################################
 
 import os
 import subprocess
 
-######################### CLASSES - START - ############################
+####################### PUBLIC CLASSES - START - #######################
 
 print ("WARNING: Plastimatch Python wrapper is still in alpha version!")
 
-class warp:
+
+class add:
 	
 	def __init__ (self, log_file=""):
 		self.log_file=log_file
-		
-	option={}
-		
-	warp_keys=("input","default-val","dif","dim","fixed",\
-	"input-cxt","input-dose-ast","input-dose-img","input-dose-mc",\
-	"input-dose-xio","input-ss-img","input-ss-list","interpolation",\
-	"metadata","origin","output-color_map","output-ctx","output-dicom",\
-	"output-dij","output-dose_img","output-img","output-labelmap",\
-	"output-pointset","output-prefix","output-prefix_fcsv","output-ss_img",\
-	"output-ss_list","output-type","output-vf","output-xio","patient-id",\
-	"patient-name","patient-pos","prune-empty","referenced-ct","simplify-perc",\
-	"spacing","xf","xor-contours")
 	
-	def _scan_options (self):
-		self.option=dict((k, v) for k, v in self.option.iteritems() if k in self.warp_keys)
-		opt_str=""
-		for key, value in dict.items(self.option):
-			opt_str+=" --"+key+"="+value
-		return opt_str
+	input_files=[]
+	output_file=""
+	
+	def run_add(self):
 		
-	def run_warp(self):
+		if len(self.input_files) < 2:
+			raise NameError("You must define at least two input images!")
+		
 		if self.log_file == "":
 			self.log=open(os.devnull, "w")
 		else:
 			self.log=open(self.log_file, "w")
 		
-		subprocess.call('plastimatch warp' + self._scan_options(), shell=True, stdout=self.log, stderr=self.log)
+		input_parms=""
+		
+		for file_name in self.input_files:
+			input_parms+=str(file_name)+" "
+		
+		subprocess.call('plastimatch add ' + input_parms + str(self.output_file),\
+		shell=True, stdout=self.log, stderr=self.log)
 		
 		self.log.close()
-		
+
+
+
 
 class convert:
 	
 	def __init__ (self, log_file=""):
 		self.log_file=log_file
-		
+	
 	option={}
-		
-	convert_keys=("input","default-val","dif","dim","fixed",\
+	
+	_convert_keys=("input","default-val","dif","dim","fixed",\
 	"input-cxt","input-dose-ast","input-dose-img","input-dose-mc",\
 	"input-dose-xio","input-ss-img","input-ss-list","interpolation",\
 	"metadata","origin","output-color_map","output-ctx","output-dicom",\
@@ -64,25 +61,93 @@ class convert:
 	"patient-name","patient-pos","prune-empty","referenced-ct","simplify-perc",\
 	"spacing","xf","xor-contours")
 	
-	def _scan_options (self):
-		self.option=dict((k, v) for k, v in self.option.iteritems() if k in self.convert_keys)
-		opt_str=""
-		for key, value in dict.items(self.option):
-			opt_str+=" --"+key+"="+value	
-		return opt_str
-		
 	def run_convert(self):
 		if self.log_file == "":
 			self.log=open(os.devnull, "w")
 		else:
 			self.log=open(self.log_file, "w")
 		
-		subprocess.call('plastimatch convert' + self._scan_options(), shell=True, stdout=self.log, stderr=self.log)
+		subprocess.call('plastimatch convert ' + _scan_options(self.option, self._convert_keys),\
+		shell=True, stdout=self.log, stderr=self.log)
 		
 		self.log.close()
 
 
-class register ():
+
+
+class diff:
+	
+	def __init__ (self, log_file=""):
+		self.log_file=log_file
+	
+	input_files=[]
+	output_file=""
+	
+	def run_diff(self):
+		
+		if len(self.input_files) != 2:
+			raise NameError("You must define two input images!")
+		
+		if self.log_file == "":
+			self.log=open(os.devnull, "w")
+		else:
+			self.log=open(self.log_file, "w")
+		
+		subprocess.call('plastimatch diff ' + str(self.input_files[0]) + " " + str(self.input_files[1])\
+		+" " + str(self.output_file), shell=True, stdout=self.log, stderr=self.log)
+		
+		self.log.close()
+
+
+
+
+class fill:
+	
+	def __init__ (self, log_file=""):
+		self.log_file=log_file
+	
+	option={}
+	
+	_fill_keys=("input", "mask", "mask-value", "output", "output-format", "output-type")
+	
+	def run_fill(self):
+		if self.log_file == "":
+			self.log=open(os.devnull, "w")
+		else:
+			self.log=open(self.log_file, "w")
+		
+		subprocess.call('plastimatch fill ' + _scan_options(self.option, self._fill_keys),\
+		shell=True, stdout=self.log, stderr=self.log)
+		
+		self.log.close()
+
+
+
+
+class mask:
+	
+	def __init__ (self, log_file=""):
+		self.log_file=log_file
+	
+	option={}
+	
+	_mask_keys=("input", "mask", "mask-value", "output", "output-format", "output-type")
+	
+	def run_mask(self):
+		if self.log_file == "":
+			self.log=open(os.devnull, "w")
+		else:
+			self.log=open(self.log_file, "w")
+		
+		subprocess.call('plastimatch mask ' + _scan_options(self.option, self._mask_keys),\
+		shell=True, stdout=self.log, stderr=self.log)
+		
+		self.log.close()
+
+
+
+
+class register:
 	
 	def __init__ (self, par_file="", log_file=""):
 		self.par_file=par_file
@@ -93,47 +158,47 @@ class register ():
 	"histoeq","grad_tol","grid_spac","max_its","max_step","metric","mi_histogram_bins",\
 	"min_its","min_step","num_saples","regularization_lambda","res","ss","ss_fixed",\
 	"ss_moving","threading","xform_in","xform_out","vf_out","img_out","img_out_fmt","img_out_type")
-		
+	
 	_global_keys=("fixed","moving","xform_in","xform_out","vf_out","img_out",\
 	"img_out_fmt","img_out_type","background_max")
-
+	
 	stages=[]
 	_global_stage_added=False
 	
 	def add_global_stage(self):
 		if self.par_file=="" or os.path.exists(self.par_file):
-			print ("WARNING:GLOBAL STAGE NOT ADDED! You have to define a new parameters file name")
+			raise NameError("GLOBAL STAGE NOT ADDED! You have to define a new parameters file name")
 		else:
 			if self._global_stage_added==False:
 				self.stages=[{}]+self.stages
 				self._global_stage_added=True
 			else:
-				print ("The global stage already exists!")
-		
+				raise NameError("The global stage already exists!")
+	
 	def add_stage(self):
 		if self.par_file=="" or os.path.exists(self.par_file):
-			print ("WARNING:STAGE NOT ADDED! You have to define a new parameters file name")
+			raise NameError("STAGE NOT ADDED! You have to define a new parameters file name")
 		else:
 			self.stages+=[{}]
 	
 	def delete_stage(self, stage_number):
 		if self.par_file=="" or os.path.exists(self.par_file):
-			print ("WARNING:STAGE NOT DELETED! You have to define a new parameters file name")
+			raise NameError("STAGE NOT DELETED! You have to define a new parameters file name")
 		else:
 			if stage_number != 0:
 				del self.stages[stage_number]
 			else:
-				print ("WARNING: GLOBAL STAGE NOT DELETED! You can not delete the global stage")
+				raise NameError("GLOBAL STAGE NOT DELETED! You can not delete the global stage")
 	
 	def run_registration(self):
 		if not os.path.exists(self.par_file) and self.par_file!="":
 			f=open(self.par_file, "w")			
 			for stage_index, stage in enumerate(self.stages):
 				if stage_index==0:
-					stage=dict((k, v) for k, v in stage.iteritems() if k in self._global_keys)
+					stage=_clean_parms(stage, self._global_keys)
 					f.write("[GLOBAL]\n")
 				else:
-					stage=dict((k, v) for k, v in stage.iteritems() if k in self._stage_keys)
+					stage=_clean_parms(stage, self._stage_keys)
 					f.write("\n[STAGE]\n")
 				
 				for key, value in dict.items(stage):
@@ -150,6 +215,86 @@ class register ():
 			subprocess.call('plastimatch register ' + self.par_file, shell=True, stdout=self.log, stderr=self.log)
 			self.log.close()
 		else:
-			print ("WARNING:REGISTRATION NOT EXECUTED! You have to define a new parameters file name")
+			raise NameError("REGISTRATION NOT EXECUTED! You have to define a new parameters file name")
 
-########################## CLASSES - END - #############################
+
+
+
+class resample:
+	
+	def __init__ (self, log_file=""):
+		self.log_file=log_file
+		
+	option={}
+	
+	_resample_keys=("default-value", "dim", "fixed", "input", "interpolation",\
+	"origin", "output", "output-type", "spacing", "subsample")
+	
+	def run_resample(self):
+		if self.log_file == "":
+			self.log=open(os.devnull, "w")
+		else:
+			self.log=open(self.log_file, "w")
+		
+		subprocess.call('plastimatch resample ' + _scan_options(self.option, self._resample_keys),\
+		shell=True, stdout=self.log, stderr=self.log)
+		
+		self.log.close()
+
+
+
+
+class warp:
+	
+	def __init__ (self, log_file=""):
+		self.log_file=log_file
+		
+	option={}
+	
+	_warp_keys=("input","default-val","dif","dim","fixed",\
+	"input-cxt","input-dose-ast","input-dose-img","input-dose-mc",\
+	"input-dose-xio","input-ss-img","input-ss-list","interpolation",\
+	"metadata","origin","output-color_map","output-ctx","output-dicom",\
+	"output-dij","output-dose_img","output-img","output-labelmap",\
+	"output-pointset","output-prefix","output-prefix_fcsv","output-ss_img",\
+	"output-ss_list","output-type","output-vf","output-xio","patient-id",\
+	"patient-name","patient-pos","prune-empty","referenced-ct","simplify-perc",\
+	"spacing","xf","xor-contours")
+	
+	def run_warp(self):
+		if self.log_file == "":
+			self.log=open(os.devnull, "w")
+		else:
+			self.log=open(self.log_file, "w")
+		
+		subprocess.call('plastimatch warp ' + _scan_options(self.option, self._warp_keys),\
+		shell=True, stdout=self.log, stderr=self.log)
+		
+		self.log.close()
+
+
+
+
+####################### PUBLIC CLASSES - END - #########################
+
+
+
+
+#################### UTILITY FUNCTION - START - ########################
+############ PRIVATE UTILITY FUNCTION, NOT FOR PUBLIC USE ##############
+
+def _clean_parms (d, t):
+		return dict((k, v) for k, v in d.iteritems() if k in t)
+
+
+def _scan_options (d, t):
+		d=_clean_parms(d, t)
+		opt_str=""
+		for key, value in dict.items(d):
+			opt_str+=" --"+key+"="+value
+		return opt_str
+
+
+############ PRIVATE UTILITY FUNCTION, NOT FOR PUBLIC USE ##############
+##################### UTILITY FUNCTION - END - #########################
+
--- extra/python/pypla/pypla_test.py
+++ extra/python/pypla/pypla_test.py
@@ -2,32 +2,55 @@
 ## This is a test file for the Plastimatch Python wrapper
 ## Usage: python pypla_test.py
 ## Author: Paolo Zaffino  (p.zaffino@yahoo.it)
-## Rev 1
+## Rev 2
 ## NOT TESTED ON PYTHON 3
 ########################################################################
 
-
 import plastimatch as plm
-"""
-## EXAMPLE TO WARP AN IMAGE
-warp=plm.warp() ## Another way to create this objcet is: warp=plm.warp(log_file="log_warp.txt")
-warp.log_file="log_warp.txt" ## Log file is not indispensable
-warp.option['input']="fix.mha"
-warp.option['output-img']="out.mha"
-warp.option['xf']="vf.mha"
-warp.run_warp()
+
+## EXAMPLE TO ADD TWO (OR MORE) IMAGES
+sum=plm.add() #Another way to create this objcet is: sum=plm.add(log_file="add_log.txt")
+sum.log_file="add_log.txt" ## Log file is not indispensable
+sum.input_files=["img1.mha","img2.mha"] ## You must define at least two images
+sum.output_file="sum_img.mha"
+sum.run_add()
 
 ## EXAMPLE TO CONVERT AN IMAGE
-conv=plm.convert() ## Another way to create this objcet is: conv=plm.convert(log_file="log_warp.txt")
-conv.log_file="log_convert.txt" ## Log file is not indispensable
+conv=plm.convert() ## Another way to create this objcet is: conv=plm.convert(log_file="conv_log.txt")
+conv.log_file="convert_log.txt" ## Log file is not indispensable
 conv.option['input']="dicom_dir"
 conv.option['output-img']="img_from_dicom.mha"
-conv.run_convert()"""
+conv.run_convert()
+
+## EXAMPLE TO SUBTRACT TWO IMAGES
+sub=plm.diff() #Another way to create this objcet is: sub=plm.diff(log_file="diff_log.txt")
+sub.log_file="diff_log.txt" ## Log file is not indispensable
+sub.input_files=["img1.mha","img2.mha"] ## You must define two images of the same dimension
+sub.output_file="sub_img.mha"
+sub.run_diff()
+
+## EXAMPLE TO FILL AN IMAGE
+fill=plm.fill() #Another way to create this objcet is: fill=plm.fill(log_file="fill_log.txt")
+fill.log_file="fill_log.txt" ## Log file is not indispensable
+fill.option['input']="fix.mha"
+fill.option['mask']="mask_fix.mha"
+fill.option['mask-value']="-1200"
+fill.option['output']="out.mha"
+fill.run_fill()
+
+## EXAMPLE TO MASK AN IMAGE
+mask=plm.mask() #Another way to create this objcet is: mask=plm.mask(log_file="mask_log.txt")
+mask.log_file="mask_log.txt" ## Log file is not indispensable
+mask.option['input']="img.mha"
+mask.option['mask']="mask.mha"
+mask.option['mask-value']="-1000"
+mask.option['output']="out.mha"
+mask.run_mask()
 
 ## EXAMPLE TO REGISTER AN IMAGE
-reg=plm.register()## Another way to create this objcet is: reg=plm.register(par_file="par.txt", log_file="log.txt")
+reg=plm.register()## Another way to create this objcet is: reg=plm.register(par_file="par.txt", log_file="reg_log.txt")
 reg.par_file="par.txt" ## Parameters file is indispensable, it could be also a inexistent file (it will be created)
-reg.log_file="log.txt" ## Log file is not indispensable
+reg.log_file="reg_log.txt" ## Log file is not indispensable
 reg.add_global_stage() ## This section is needed only if the paramaters file does not exist - START - 
 reg.stages[0]["fixed"]="fix.mha"
 reg.stages[0]["moving"]="mov.mha"
@@ -59,3 +82,19 @@ reg.stages[3]["grid_spac"]="10 10 10"
 reg.stages[3]["res"]="1 1 1" ## This section is needed only if the paramaters file does not exist - END -
 reg.delete_stage(3) ## Deletes the last stage
 reg.run_registration()
+
+## EXAMPLE TO RESAMPLE AN IMAGE
+res=plm.resample() #Another way to create this objcet is: res=plm.resample(log_file="res_log.txt")
+res.log_file="res_log.txt" ## Log file is not indispensable
+res.option['input']="img1.mha"
+res.option['fixed']="img2.mha"
+res.option['output']="res_img.mha"
+res.run_resample()
+
+## EXAMPLE TO WARP AN IMAGE
+warp=plm.warp() ## Another way to create this objcet is: warp=plm.warp(log_file="warp_log.txt")
+warp.log_file="warp_log.txt" ## Log file is not indispensable
+warp.option['input']="fix.mha"
+warp.option['output-img']="out.mha"
+warp.option['xf']="vf.mha"
+warp.run_warp()
