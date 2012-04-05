2909e57aa8e150868dd309864bcd1165fc14d926
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-04-05 00:22:25 +0000

    Fix paths for gelato cdash scripts

--- extra/cdash/gelato_2.cmake
+++ extra/cdash/gelato_2.cmake
@@ -1,13 +1,13 @@
 SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/work/plastimatch")
 SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/build/nightly/gelato_2/plastimatch")
-SET (CTEST_CMAKE_COMMAND "/usr/bin/cmake")
-SET (CTEST_COMMAND "/usr/bin/ctest -D Nightly")
+SET (CTEST_CMAKE_COMMAND "/usr/local/bin/cmake")
+SET (CTEST_COMMAND "/usr/local/bin/ctest -D Nightly")
 SET (CTEST_INITIAL_CACHE "
 //Name of generator.
 CMAKE_GENERATOR:INTERNAL=Unix Makefiles
 
 //Name of the build
-BUILDNAME:STRING=02-lin64-PiSr-Cd-gcc4.3.4
+BUILDNAME:STRING=02-lin64-PiSr-Cd-gcc4.4.5
 
 //Name of the computer/site where compile is being run
 SITE:STRING=gelato
