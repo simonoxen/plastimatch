3ace526c30919acf09c8b330f50d9913e4b282e1
James A Shackleford <shack@linux.com>
2012-04-05 20:16:15 +0000

    Add hourly build to zerowing.

--- extra/cdash/zerowing-continuous.cmake
+++ extra/cdash/zerowing-continuous.cmake
@@ -1,13 +1,13 @@
-SET (CTEST_SOURCE_DIRECTORY "D:/src/plastimatch-nightly")
-SET (CTEST_BINARY_DIRECTORY "D:/build/plastimatch-nightly")
+SET (CTEST_SOURCE_DIRECTORY "D:/src/plastimatch-continuous")
+SET (CTEST_BINARY_DIRECTORY "D:/build/plastimatch-continuous")
 SET (CTEST_CMAKE_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
-SET (CTEST_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Nightly")
+SET (CTEST_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Continuous")
 SET (CTEST_INITIAL_CACHE "
 //Name of generator.
 CMAKE_GENERATOR:INTERNAL=Visual Studio 8 2005
 
 //Name of the build
-BUILDNAME:STRING=win7-Pisr-Cd-vse8-cuda4.1
+BUILDNAME:STRING=win7-Pisr-Cd-vse8-cuda4.1-hourly
 
 //Name of the computer/site where compile is being run
 SITE:STRING=zerowing
