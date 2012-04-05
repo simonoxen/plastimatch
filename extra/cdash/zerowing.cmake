e7c42882323cd146d334cde1d399c3c9f9b52846
James A Shackleford <shack@linux.com>
2012-04-05 18:02:11 +0000

    Add nightly build config for zerowing

---
+++ extra/cdash/zerowing.cmake
@@ -0,0 +1,24 @@
+SET (CTEST_SOURCE_DIRECTORY "D:/src/plastimatch-nightly")
+SET (CTEST_BINARY_DIRECTORY "D:/build/plastimatch-nightly")
+SET (CTEST_CMAKE_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
+SET (CTEST_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Nightly")
+SET (CTEST_INITIAL_CACHE "
+//Name of generator.
+CMAKE_GENERATOR:INTERNAL=Visual Studio 8 2005
+
+//Name of the build
+BUILDNAME:STRING=win7-Pisr-Cd-vse8
+
+//Name of the computer/site where compile is being run
+SITE:STRING=zerowing
+
+//Build without shared libraries.
+BUILD_SHARED_LIBS:BOOL=OFF
+
+//Don't Build REG-2-3 for now
+PLM_CONFIG_DISABLE_REG23:BOOL=OFF
+
+//Path to the CVS
+CVSCOMMAND:FILEPATH=C:/cygwin/bin/cvs.exe
+SVNCOMMAND:FILEPATH=C:/cygwin/bin/svn.exe
+")
