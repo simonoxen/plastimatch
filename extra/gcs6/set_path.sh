f8b0f4bf364b8b4fac30b1e69cbb316b24dd24f5
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-04-23 17:11:16 +0000

    Update paths

--- extra/gcs6/set_path.sh
+++ extra/gcs6/set_path.sh
@@ -7,7 +7,7 @@ case $CNAME in
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
     "gelato")
-	export PATH=$PATH:$HOME/build/plastimatch-3.20.0
+	export PATH=$PATH:$HOME/build/plastimatch-3.20.1
 	export PATH=$PATH:$HOME/work/plastimatch/extra/perl
 	;;
     "physics.mgh.harvard.edu")
