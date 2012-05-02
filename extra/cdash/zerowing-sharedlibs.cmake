f2cb68bf201a0b668cb7778bcca6f6572a2e7e90
James A Shackleford <shack@linux.com>
2012-05-02 21:54:04 +0000

    Fix issue with Zerowing building against wrong ITK

--- extra/cdash/zerowing-sharedlibs.cmake
+++ extra/cdash/zerowing-sharedlibs.cmake
@@ -1,22 +1,22 @@
-SET (CTEST_SOURCE_DIRECTORY "D:/src/plastimatch-continuous")
-SET (CTEST_BINARY_DIRECTORY "D:/build/plastimatch-continuous")
+SET (CTEST_SOURCE_DIRECTORY "D:/src/plastimatch-nightly")
+SET (CTEST_BINARY_DIRECTORY "D:/build/plastimatch-nightly")
 SET (CTEST_CMAKE_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
-SET (CTEST_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Continuous")
+SET (CTEST_COMMAND "c:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Nightly")
 SET (CTEST_INITIAL_CACHE "
-//Name of generator
+//Name of generator.
 CMAKE_GENERATOR:INTERNAL=Visual Studio 8 2005
 
 //Name of the build
-BUILDNAME:STRING=win7-Pisr-Cd-vse8-cuda4.1-hourly
+BUILDNAME:STRING=win7-Pisr-Cd-vse8-cuda4.1-DLLs
 
 //Name of the computer/site where compile is being run
 SITE:STRING=zerowing
 
 //Build without shared libraries.
-BUILD_SHARED_LIBS:BOOL=OFF
+BUILD_SHARED_LIBS:BOOL=ON
 
-//Used statically compiled ITK
-ITK_DIR:FILEPATH=D:/build/itk-3.20.1
+//Used dynamically compiled ITK
+ITK_DIR:FILEPATH=D:/build/itk-3.20.1-shared
 
 //Don't Build REG-2-3 for now
 PLM_CONFIG_DISABLE_REG23:BOOL=OFF
