d658d526bcf365489bbbaf17da4b43a390d55f98
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-05-06 21:38:52 +0000

    Add skeleton doxygen support

--- src/plastimatch/util/gamma_analysis.h
+++ src/plastimatch/util/gamma_analysis.h
@@ -9,6 +9,8 @@
 
 class Plm_image;
 
+/*! \brief This is the Gamma_parms class.
+ * How it works is a mystery. */
 class Gamma_parms {
 public:
     
@@ -21,17 +23,19 @@ public:
     Plm_image *img_out;
     Plm_image *img_out_pass;
     Plm_image *img_out_fail;
-	Plm_image *labelmap_out;
+    Plm_image *labelmap_out;
 
 public:
     Gamma_parms () {
-
-    r_tol = d_tol = gamma_max = 3;
-
+        r_tol = d_tol = gamma_max = 3;
     }
 };
 
+/*! \brief This function finds a dose threshold.  It is a global function. 
+  If we document the file (with the \file directive) or if we add this 
+  function to a group, it will show up in Doxygen. */
 C_API void find_dose_threshold (Gamma_parms *parms);
+
 C_API void do_gamma_analysis (Gamma_parms *parms);
 
 #endif
