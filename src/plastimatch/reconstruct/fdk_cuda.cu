ebe22f8091c7e21b6304ee64b507c953ea464942
James A Shackleford <shack@linux.com>
2012-05-08 21:38:27 +0000

    Fix CUDA error under Windows

--- src/plastimatch/reconstruct/fdk_cuda.cu
+++ src/plastimatch/reconstruct/fdk_cuda.cu
@@ -23,11 +23,16 @@
 #include <cuda.h>
 
 #include "plmbase.h"
-#include "plmreconstruct.h"
 #include "plmsys.h"
-#include "plmutil.h"
 
 #include "plm_math.h"
+
+/* temporary fix */
+//#include "plmreconstruct.h"
+//#include "plmutil.h"
+#include "fdk.h"
+#include "fdk_util.h"
+#include "proj_image_filter.h"
 
 #include "cuda_util.h"
 #include "fdk_cuda_p.h"
