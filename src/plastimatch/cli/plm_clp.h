517920f338715e93b7d056e685bf847020a8d4f6
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-05-08 20:56:59 +0000

    Make plm_clp into static library

--- src/plastimatch/cli/plm_clp.h
+++ src/plastimatch/cli/plm_clp.h
@@ -21,7 +21,7 @@
 typedef dlib::cmd_line_parser<char>::check_1a_c Clp;
 
 namespace dlib {
-class plastimatch1_EXPORT Plm_clp
+class Plm_clp
     : public cmd_line_parser<char>::check_1a_c 
 {
     public:
