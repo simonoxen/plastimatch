0b842d9cf4961ad800c3f1f82bfacf0b1ab4c25a
James A Shackleford <shack@linux.com>
2012-05-08 19:28:41 +0000

    Add missing files

---
+++ src/plastimatch/util/proj_image_filter.cxx
@@ -0,0 +1,15 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#include "plmutil_config.h"
+
+#include "plmbase.h"
+#include "plmutil.h"
+
+void
+proj_image_filter (Proj_image *proj)
+{
+#if (FFTW_FOUND)
+    ramp_filter (proj->img, proj->dim[0], proj->dim[1]);
+#endif
+}
---
+++ src/plastimatch/util/proj_image_filter.h
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#ifndef _proj_image_filter_h_
+#define _proj_image_filter_h_
+
+class Proj_image;
+
+void proj_image_filter (Proj_image *proj);
+
+#endif
