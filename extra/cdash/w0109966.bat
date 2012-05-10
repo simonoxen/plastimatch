72229f4f74e11c634fcb1b904684301f667e3327
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-05-10 14:48:46 +0000

    More cdash for windows

---
+++ extra/cdash/w0109966.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_3.cmake" -V
+"C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_4.cmake" -V
