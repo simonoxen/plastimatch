299380887511045b7b13333b540863d253482a80
James A Shackleford <shack@linux.com>
2012-05-15 23:23:08 +0000

    Update delayload.c for static OS X builds with OpenCL | opencl_probe almost works

--- src/plastimatch/sys/delayload.c
+++ src/plastimatch/sys/delayload.c
@@ -224,6 +224,14 @@ delayload_libplmopencl (void)
         // success
         return 1;
     }
+#elif defined (APPLE)
+
+#if defined (PLM_USE_GPU_PLUGINS)
+    printf ("OpenCL support of OS X is currently not available when building\n");
+    printf ("Plastimatch with BUILD_SHARED_LIBS enabled. Sorry.\n\n");
+    return 0;
+#endif
+    return 1;
 #else
     if (!find_lib ("libOpenCL.so")) {
         printf ("Failed to load GPU Plugins! (err: OpenCL)\n");
