6fb71f6f3f85ed43f86519f530cbddf4a2abc444
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-05-31 12:39:29 +0000

    Fix build configuration for windows S4 nightly builds

--- extra/cdash/w0109966_6.cmake
+++ extra/cdash/w0109966_6.cmake
@@ -1,5 +1,5 @@
 SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/build/nightly/src/plastimatch")
-SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/build/nightly/w0109966_5")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/build/nightly/w0109966_6")
 SET (CTEST_CMAKE_COMMAND "c:/Program Files/CMake 2.8/bin/cmake.exe")
 SET (CTEST_COMMAND "c:/Program Files/CMake 2.8/bin/ctest.exe -D Nightly")
 SET (CTEST_INITIAL_CACHE "
@@ -21,4 +21,5 @@ PLM_CONFIG_DISABLE_REG23:BOOL=ON
 //Path to the CVS
 CVSCOMMAND:FILEPATH=C:/cygwin/bin/cvs.exe
 SVNCOMMAND:FILEPATH=C:/cygwin/bin/svn.exe
+GIT_EXECUTABLE:FILEPATH=C:/cygwin/bin/git.exe
 ")
