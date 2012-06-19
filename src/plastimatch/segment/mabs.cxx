71504a3f0105c6f580d5086c88777332ce5d17c9
Amelia Arbisser <amarbisser@gmail.com>
2012-06-19 13:02:17 +0000

    implementation comments for mabs sequencer

--- src/plastimatch/segment/mabs.cxx
+++ src/plastimatch/segment/mabs.cxx
@@ -35,6 +35,8 @@ Mabs::run (const Mabs_parms& parms)
     /* Load the labeling file.  For now, we'll assume this is successful. */
     Plm_image pli (parms.labeling_input_fn);
 
+    // create the data structure for saving the automatically generated labels
+    
     /* Parse the atlas directory */
     Dir_list d (parms.atlas_dir);
     for (int i = 0; i < d.num_entries; i++) {
@@ -67,7 +69,7 @@ Mabs::run (const Mabs_parms& parms)
         int rc = regp.set_command_string (command_string);
         if (rc) {
             print_and_exit ("Failure parsing command file: %s\n",
-                parms.registration_config);
+                            parms.registration_config);
         }
 
         /* Manually set input files */
@@ -80,11 +82,26 @@ Mabs::run (const Mabs_parms& parms)
         printf ("DO_REGISTRATION_PURE\n");
         printf ("regp.num_stages = %d\n", regp.num_stages);
         do_registration_pure (&xf_out, &regd, &regp);
-
+        
         /* *** WARP STRUCTURES *** */
-
+        // do the entire label map at once
+        
         /* *** VOTING HAPPENS *** */
+        // each structure in this patient's structures
+        // for () {
+          // add this structure to the set of structures that we're segmenting
+          // atlas_structure = get the binary structure from the label map
+          // target_structure += vote(target, atlas_image, atlas_structure,
+          // like0, like1)
+        // }
 
+        /* *** THRESHOLING *** */
+        // for each of the structures we're trying to segment
+        // for () {
+          // do the final thresholding (or whatnot)
+          // }
+        // create the final labelmap
+        
         /* Don't let regd destructor delete our fixed image */
         regd.fixed_image = 0;
 
