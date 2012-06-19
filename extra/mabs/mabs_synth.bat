aca7743dd8ec762040019c6158bd9f452cd1fc08
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-06-19 20:03:59 +0000

    Add mabs synth data set

---
+++ extra/mabs/mabs_synth.bat
@@ -0,0 +1,7 @@
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos -10 --output-dicom mabs_synth/atlas/lung_m10
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos -6 --output-dicom mabs_synth/atlas/lung_m6
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos -2 --output-dicom mabs_synth/atlas/lung_m2
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos 10 --output-dicom mabs_synth/atlas/lung_p10
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos 6 --output-dicom mabs_synth/atlas/lung_p6
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos 2 --output-dicom mabs_synth/atlas/lung_p2
+plastimatch synth --pattern lung --dim "200 200 75" --volume-size "500 500 200" --lung-tumor-pos 0 --output-dicom mabs_synth/testcase
