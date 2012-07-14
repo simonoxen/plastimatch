5d75bfd4b63c29a62f60bd83eae50259d56269b2
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-07-14 15:00:54 +0000

    Fix ora build problem when compiling with slicer vtk

--- cmake/FindSlicer.cmake
+++ cmake/FindSlicer.cmake
@@ -90,59 +90,8 @@ if (SLICER_FOUND)
     endif ()
   endif ()
 
-  # Load the Slicer configuration files
-  #
-  # Strangely, the Slicer use file (both Slicer 3 and Slicer 4) 
-  # appends the Slicer directories to our include path.  
-  # This causes nvcc.exe to barf on windows, so we can't let it do this.
-  #
-  # Equally strangely, in Slicer 4, the use file sets our C/CXX flags. 
-  # These cause nvcc + gcc-4.3 to barf because the flags are for gcc-4.5
-  if (Slicer_USE_FILE)
-    get_directory_property (OLD_INCLUDE_DIR INCLUDE_DIRECTORIES)
-    set_directory_properties (PROPERTIES INCLUDE_DIRECTORIES "")
-    set (OLD_CFLAGS ${CMAKE_C_FLAGS})
-    set (OLD_CXXFLAGS ${CMAKE_CXX_FLAGS})
-
-    include ("${Slicer_USE_FILE}")
-    get_directory_property (SLICER_INCLUDE_DIRS INCLUDE_DIRECTORIES)
-    set_directory_properties (PROPERTIES INCLUDE_DIRECTORIES
-      "${OLD_INCLUDE_DIR}")
-    set (CMAKE_C_FLAGS "${OLD_CFLAGS}" CACHE STRING "CMake CXX Flags" FORCE)
-    set (CMAKE_CXX_FLAGS "${OLD_CXXFLAGS}" CACHE STRING "CMake CXX Flags" FORCE)
-  endif ()
-
-  if (SLICER_IS_SLICER3)
-    # Set reasonable default install prefix and output paths
-    # (after setting Slicer3_DIR, delete CMAKE_INSTALL_PREFIX and re-configure)
-    slicer3_set_default_install_prefix_for_external_projects ()
-  else ()
-
-    # Again, prevent Slicer 4 from overwriting C/CXX flags
-    set (OLD_CFLAGS ${CMAKE_C_FLAGS})
-    set (OLD_CXXFLAGS ${CMAKE_CXX_FLAGS})
-
-    # 2012-01-10: JC says these aren't needed any more
-    ## Slicer 4: include missing cmake scripts (Slicer3 stuff)
-    include (Slicer3PluginsMacros)
-    ## Slicer 4: include missing cmake scripts (loadable module stuff)
-    include (vtkMacroKitPythonWrap)
-    include (ctkMacroWrapPythonQt)
-    include (ctkMacroCompilePythonScript)
-    ## Slicer 4: this seems to be needed too
-    include(${GenerateCLP_USE_FILE})
-
-    set (CMAKE_C_FLAGS "${OLD_CFLAGS}" CACHE STRING "CMake CXX Flags" FORCE)
-    set (CMAKE_CXX_FLAGS "${OLD_CXXFLAGS}" CACHE STRING "CMake CXX Flags" FORCE)
-  endif ()
-
   ## Always set shared libs on
   set (BUILD_SHARED_LIBS ON)
-else ()
-  ## Restore old values 
-  #set (ITK_DIR ${ITK_DIR_OLD})
-  #set (QT_QMAKE_EXECUTABLE ${QT_QMAKE_EXECUTABLE_OLD})
-  #set (VTK_DIR ${VTK_DIR_OLD})
 endif ()
 message (STATUS "BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 
