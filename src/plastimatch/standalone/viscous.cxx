edacb87e0d93f88051b6ca4ff7de3af45995834a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-09-04 14:42:29 +0000

    Fix build error with viscous

--- src/plastimatch/standalone/viscous.cxx
+++ src/plastimatch/standalone/viscous.cxx
@@ -2,10 +2,10 @@
    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
    ----------------------------------------------------------------------- */
 #include "plm_config.h"
-#include "viscous_main.h"
+#include "viscous.h"
 
 int
 main (int argc, char *argv[])
 {
-    return viscous_main (argc, argv);
+    return viscous (argc, argv);
 }
