017b5a3c25c13db9754a1eb9c022121f66bbffff
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-11-28 14:16:39 +0000

    Remove slicer tests from dashboard

--- extra/cdash/w0109966.bat
+++ extra/cdash/w0109966.bat
@@ -2,5 +2,5 @@
 "C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_2.cmake" -V
 "C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_3.cmake" -V
 "C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_4.cmake" -V
-"C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_5.cmake" -V
-"C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_6.cmake" -V
+@rem "C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_5.cmake" -V
+@rem "C:\Program Files\CMake 2.8\bin\ctest.exe" -S "c:\gcs6\work\plastimatch\extra\cdash\w0109966_6.cmake" -V
