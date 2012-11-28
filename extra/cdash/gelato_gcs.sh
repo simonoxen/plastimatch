da0a979f70474deb604e1784312f8790fe4b0e6d
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-11-28 14:42:13 +0000

    Update cdash script

--- extra/cdash/gelato_gcs.sh
+++ extra/cdash/gelato_gcs.sh
@@ -1,2 +1,2 @@
-/usr/bin/ctest -S /Users/gcs6/work/plastimatch/extra/cdash/gelato_01.cmake -V > /Users/gcs6/build/nightly/gelato_01/tests.log 2>&1
-/usr/bin/ctest -S /Users/gcs6/work/plastimatch/extra/cdash/gelato_03.cmake -V > /Users/gcs6/build/nightly/gelato_03/tests.log 2>&1
+/usr/local/bin/ctest -S /PHShome/gcs6/work/plastimatch/extra/cdash/gelato_01.cmake -V > /PHShome/gcs6/build/nightly/gelato_01/tests.log 2>&1
+/usr/local/bin/ctest -S /PHShome/gcs6/work/plastimatch/extra/cdash/gelato_03.cmake -V > /PHShome/gcs6/build/nightly/gelato_03/tests.log 2>&1
