e5534bd7a1497fc543348341d10c4b81d5fbe6d7
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2012-12-20 01:10:37 +0000

    Add possibility to set gradient magnitude tolerance for rsg optimizer

--- src/plastimatch/register/itk_optimizer.cxx
+++ src/plastimatch/register/itk_optimizer.cxx
@@ -296,6 +296,7 @@ set_optimization_rsg (RegistrationType::Pointer registration,
     optimizer->SetMaximumStepLength(stage->max_step);
     optimizer->SetMinimumStepLength(stage->min_step);
     optimizer->SetNumberOfIterations(stage->max_its);
+    optimizer->SetGradientMagnitudeTolerance (stage->grad_tol);
     registration->SetOptimizer(optimizer);
 }
 
