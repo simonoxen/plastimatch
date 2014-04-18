c6c4f19cb8899dd52a67c19d8f19f7449d843d1f
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2014-04-18 21:38:48 +0000

    Tell Qt designer plugin to be built into designer directory

--- src/plastimatch/test/qt_test.cxx
+++ src/plastimatch/test/qt_test.cxx
@@ -2,12 +2,16 @@
    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
    ----------------------------------------------------------------------- */
 #include "plm_config.h"
+#include <iostream>
 #include <QApplication>
 #include <QLabel>
 
 int
 main (int argc, char **argv)
 {
+    // Display designer path
+    QStringList paths = QCoreApplication::libraryPaths(); for (QStringList::iterator it = paths.begin(); it!=paths.end(); it++) { std::cout << "Looking for plugins at path: " << it->toStdString() << std::endl; } 
+
     QApplication app(argc, argv);
     QLabel *label = new QLabel("Hello World!");
   
