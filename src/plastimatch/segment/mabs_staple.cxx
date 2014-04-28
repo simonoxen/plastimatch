a4dd38ad7cb11f51426bde40b913c4d89f746a05
Paolo Zaffino <p.zaffino@yahoo.it>
2014-04-28 15:45:38 +0000

    [MABS] add staple skeleton

---
+++ src/plastimatch/segment/mabs_staple.cxx
@@ -0,0 +1,57 @@
+/* -----------------------------------------------------------------------
+ *    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+ *       ----------------------------------------------------------------------- */
+#include "plmsegment_config.h"
+
+#include "itkImage.h"
+#include <itkSTAPLEImageFilter.h>
+
+#include "mabs_staple.h"
+
+Mabs_staple::Mabs_staple() {
+
+    this->foreground_val = 1;
+
+}
+
+
+Mabs_staple::~Mabs_staple() {
+
+    this->structures.clear();
+
+}
+
+
+void
+Mabs_staple::add_input_structure(Plm_image::Pointer& structure) {
+
+    this->structures.push_back(structure);
+
+}
+
+
+void
+Mabs_staple::run() {
+
+    typedef float PixelComponentType;
+    const unsigned int Dimension = 3;
+    typedef itk::Image< PixelComponentType, Dimension > ImageType;
+    typedef itk::STAPLEImageFilter< ImageType, ImageType > StapleType;
+    StapleType::Pointer staple = StapleType::New();
+
+    std::list<Plm_image::Pointer>::iterator stru_it;
+    int i;
+    for (stru_it = this->structures.begin(), i=0;
+         stru_it != this->structures.end(); stru_it++, i++) {
+
+        staple->SetInput(i, (*stru_it)->itk_float());
+
+    }
+
+    staple->SetForegroundValue(this->foreground_val);
+    staple->Update();
+
+    this->output_img = Plm_image::New(staple->GetOutput());
+
+}
+
---
+++ src/plastimatch/segment/mabs_staple.h
@@ -0,0 +1,30 @@
+/* -----------------------------------------------------------------------
+ *    See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+ *       ----------------------------------------------------------------------- */
+#ifndef _mabs_staple_h_
+#define _mabs_staple_h_
+
+#include "plmsegment_config.h"
+
+#include <iostream>
+#include <list>
+
+#include "plm_image.h"
+
+class PLMSEGMENT_API Mabs_staple {
+
+public:
+    Mabs_staple();
+    ~Mabs_staple();
+    void add_input_structure(Plm_image::Pointer&);
+    void run();
+
+public:
+    std::list<Plm_image::Pointer> structures;
+    int foreground_val;
+
+    Plm_image::Pointer output_img;
+
+};
+
+#endif /* #ifndef _mabs_staple_h_ */
