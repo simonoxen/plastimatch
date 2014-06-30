b4d739a50df4462e5448cc04541788a72a3a27af
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2014-06-30 13:26:00 +0000

    Split off double_align8 into a separate file

--- src/plastimatch/register/bspline.h
+++ src/plastimatch/register/bspline.h
@@ -12,21 +12,6 @@
 #include "bspline_regularize.h"
 #include "bspline_score.h"
 
-/* JAS 2011.07.23
- * The following is a fix that allows us to more selectively enforce
- * the -malign-double compatibility required by object files compiled
- * by nvcc.  Any structures that are used by both nvcc compiled files
- * and gcc/g++ compiled files should use this.  The reason we do not
- * simply pass -malign-double to gcc/g++ in order to achieve this
- * compatibility is because Slicer's ITK does not come compiled with
- * the -malign-double flag on 32-bit systems... so, believe it or not
- * this might be the cleanest solution */
-#if (__GNUC__) && (MACHINE_IS_32_BIT) && (CUDA_FOUND)
-    typedef double double_align8 __attribute__ ((aligned(8)));
-#else 
-    typedef double double_align8;
-#endif
-
 class Bspline_landmarks;
 class Bspline_mi_hist_set;
 class Bspline_parms;
@@ -58,15 +43,6 @@ public:
 };
 
 
-PLMREGISTER_C_API Bspline_state* bspline_state_create (
-    Bspline_xform *bxf, 
-    Bspline_parms *parms
-);
-PLMREGISTER_C_API void bspline_state_destroy (
-    Bspline_state *bst,
-    Bspline_parms *parms,
-    Bspline_xform *bxf
-);
 PLMREGISTER_C_API Volume* bspline_compute_vf (const Bspline_xform* bxf);
 void bspline_display_coeff_stats (Bspline_xform* bxf);
 PLMREGISTER_C_API void bspline_score (Bspline_optimize_data *bod);
--- src/plastimatch/register/bspline_parms.h
+++ src/plastimatch/register/bspline_parms.h
@@ -7,21 +7,7 @@
 #include "plmregister_config.h"
 #include <string>
 #include "bspline_mi_hist.h"
-
-/* JAS 2011.07.23
- * The following is a fix that allows us to more selectively enforce
- * the -malign-double compatibility required by object files compiled
- * by nvcc.  Any structures that are used by both nvcc compiled files
- * and gcc/g++ compiled files should use this.  The reason we do not
- * simply pass -malign-double to gcc/g++ in order to achieve this
- * compatibility is because Slicer's ITK does not come compiled with
- * the -malign-double flag on 32-bit systems... so, believe it or not
- * this might be the cleanest solution */
-#if (__GNUC__) && (MACHINE_IS_32_BIT) && (CUDA_FOUND)
-    typedef double double_align8 __attribute__ ((aligned(8)));
-#else 
-    typedef double double_align8;
-#endif
+#include "double_align8.h"
 
 enum BsplineOptimization {
     BOPT_LBFGSB,
