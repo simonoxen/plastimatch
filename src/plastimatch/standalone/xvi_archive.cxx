bbd9adad81797236080b20062e746cd9455005ff
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2014-10-28 23:21:07 +0000

    Crude attempt at making SRO from XVI data

--- src/plastimatch/standalone/xvi_archive.cxx
+++ src/plastimatch/standalone/xvi_archive.cxx
@@ -23,6 +23,7 @@
 #include "INIReader.h"
 
 #include "plm_config.h"
+#include "dcmtk_sro.h"
 #include "dir_list.h"
 #include "file_util.h"
 #include "path_util.h"
@@ -51,21 +52,46 @@ do_xvi_archive (Xvi_archive_parms *parms)
     std::string patient_images_dir = compose_filename (
         patient_dir, "IMAGES");
 
-    Dir_list image_list (patient_images_dir);
+    /* Load one of the reference CTs */
+    Dir_list ct_set_dir (patient_ct_set_dir);
+    if (ct_set_dir.num_entries == 0) {
+        printf ("Error.  No CT_SET found.\n");
+        return;
+    }
+    Rt_study reference_study;
+    std::string reference_uid;
+    for (int i = 0; i < ct_set_dir.num_entries; i++) {
+        if (ct_set_dir.entries[i][0] == '.') {
+            continue;
+        }
+        std::string fn = string_format ("%s/%s",
+            patient_ct_set_dir.c_str(), ct_set_dir.entries[i]);
+        if (is_directory (fn)) {
+            printf ("Loaded reference study (%s)\n", fn.c_str());
+            reference_study.load_image (fn);
+            reference_uid = ct_set_dir.entries[i];
+            break;
+        }
+    }
+    if (!reference_study.have_image()) {
+        printf ("Error.  No reference CT loaded.\n");
+        return;
+    }
 
-
-    if (image_list.num_entries == 0) {
+    Dir_list images_dir (patient_images_dir);
+    if (images_dir.num_entries == 0) {
         printf ("Error.  No images found.\n");
+        return;
     }
 
     /* For potential CBCT image */
-    for (int i = 0; i < image_list.num_entries; i++) {
-        if (image_list.entries[i][0] == '.') {
+    for (int i = 0; i < images_dir.num_entries; i++) {
+        if (images_dir.entries[i][0] == '.') {
             continue;
         }
         /* Find pertinent filenames */
         std::string recon_dir = string_format ("%s/%s/Reconstruction",
-            patient_images_dir.c_str(), image_list.entries[i]);
+            patient_images_dir.c_str(), images_dir.entries[i]);
         Dir_list recon_list (recon_dir);
         std::string scan_fn, recon_ini_fn, recon_xvi_fn;
         for (int j = 0; j < recon_list.num_entries; j++) {
@@ -91,20 +117,33 @@ do_xvi_archive (Xvi_archive_parms *parms)
 
         /* Load the INI file */
         INIReader recon_ini (recon_ini_fn);
-        printf ("name = %s^%s\n", 
-            recon_ini.Get ("IDENTIFICATION", "LastName", "").c_str(),
-            recon_ini.Get ("IDENTIFICATION", "FirstName", "").c_str());
+        std::string cbct_ref_uid = 
+            recon_ini.Get ("IDENTIFICATION", "ReferenceUID", "");
+        std::string patient_name = 
+            string_format ("%s^%s",
+                recon_ini.Get ("IDENTIFICATION", "LastName", "").c_str(),
+                recon_ini.Get ("IDENTIFICATION", "FirstName", "").c_str());
+
+        printf ("name = %s\n", patient_name.c_str());
+        printf ("reference_uid = %s\n", cbct_ref_uid.c_str());
+
+        /* Verify if the file belongs to this reference CT */
+        if (cbct_ref_uid != reference_uid) {
+            printf ("Reference UID mismatch.  Skipping.\n");
+            continue;
+        }
 
         /* Load the INI.XVI file */
         INIReader recon_xvi (recon_xvi_fn);
-        printf ("xform = %s\n", recon_xvi.Get ("ALIGNMENT", 
-                "OnlineToRefTransformCorrection", "").c_str());
+        std::string registration_string = 
+            recon_xvi.Get ("ALIGNMENT", "OnlineToRefTransformCorrection", "");
+        printf ("xform = %s\n", registration_string.c_str());
 
         /* Load the .SCAN */
-        Rt_study rt_study;
-        rt_study.load_image (scan_fn);
+        Rt_study cbct_study;
+        cbct_study.load_image (scan_fn);
 
-        if (!rt_study.have_image()) {
+        if (!cbct_study.have_image()) {
             printf ("ERROR: decompression failure with patient %s\n",
                 parms->patient_id.c_str());
             exit (1);
@@ -112,10 +151,43 @@ do_xvi_archive (Xvi_archive_parms *parms)
 
         /* Write the DICOM image */
         std::string output_dir = string_format (
-            "cbct_output/%s", image_list.entries[i]);
-        rt_study.save_dicom (output_dir);
+            "cbct_output/%s", images_dir.entries[i]);
+        cbct_study.save_dicom (output_dir);
 
         /* Create the DICOM SRO */
+        Xform::Pointer xf = Xform::New();
+        AffineTransformType::ParametersType xfp(12);
+        float xvip[16];
+        int rc = sscanf (registration_string.c_str(), 
+            "%f %f %f %f %f %f %f %f "
+            "%f %f %f %f %f %f %f %f",
+            &xvip[0], &xvip[1], &xvip[2], &xvip[3], 
+            &xvip[4], &xvip[5], &xvip[6], &xvip[7], 
+            &xvip[8], &xvip[9], &xvip[10], &xvip[11], 
+            &xvip[12], &xvip[13], &xvip[14], &xvip[15]);
+        if (rc != 16) {
+            printf ("Error parsing transform string.\n");
+            exit (1);
+        }
+        xfp[0] = xvip[0];
+        xfp[1] = xvip[1];
+        xfp[2] = xvip[2];
+        xfp[3] = xvip[4];
+        xfp[4] = xvip[5];
+        xfp[5] = xvip[6];
+        xfp[6] = xvip[8];
+        xfp[7] = xvip[9];
+        xfp[8] = xvip[10];
+        xfp[9] = xvip[12];
+        xfp[10] = xvip[13];
+        xfp[11] = xvip[14];
+        xf->set_aff (xfp);
+
+        Dcmtk_sro::save (
+            xf,
+            reference_study.get_rt_study_metadata (),
+            cbct_study.get_rt_study_metadata (),
+            output_dir);
 
         //break;
     }
