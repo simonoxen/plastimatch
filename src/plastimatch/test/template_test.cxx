32994bbc0bb0dc49d20683dc21adc83e8d97a936
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2014-12-11 03:57:40 +0000

    Add test program for exploring template solutions

---
+++ src/plastimatch/test/template_test.cxx
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+/* Simple test */
+typedef void (*B) (int);
+
+void c (int x) {
+    printf ("x = %d\n", x);
+}
+
+template <B b> void a (int x) {
+    b(x);
+}
+
+/* More complex */
+// This is illegal, typedef's can't be templated
+// typedef template void<F> (*E) (F);
+//void g<int> (int x) {
+//    printf ("x = %d\n", x);
+//}
+
+template <typename F>
+class G
+{
+public:
+    static void g (F x) {
+        printf ("x = %d\n", x);
+    }
+};
+
+
+template < class G > void d (int x) {
+    G::g (x);
+}
+
+int main 
+(
+    int argc,
+    char* argv[]
+)
+{
+    int x = 1;
+    a<c>(x);
+    G<int>::g (x);
+    d< G<int> >(x);
+}
