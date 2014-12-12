7cbc868a8143a89cd3d889a3b02729423ce17cba
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2014-12-12 03:26:59 +0000

    Improve statistics for test_size.pl

--- extra/time_bspline/test_size.pl
+++ extra/time_bspline/test_size.pl
@@ -6,11 +6,17 @@
 #@flavors = ( f );
 
 foreach $s (@sizes) {
-	$cmd = "plastimatch synth --output size_test_fixed.mha --pattern gauss --gauss-center \"0 0 0\" --dim=$s";
-	`$cmd`;
-	$offset = $s * 0.1;
-	$cmd = "plastimatch synth --output size_test_moving.mha --pattern gauss --gauss-center \"$offset $offset 0\" --dim=$s";
-	`$cmd`;
+    $cmd = "plastimatch synth --output size_test_fixed.mha --pattern gauss --gauss-center \"0 0 0\" --dim=$s";
+    `$cmd`;
+    $offset = $s * 0.1;
+    $cmd = "plastimatch synth --output size_test_moving.mha --pattern gauss --gauss-center \"$offset $offset 0\" --dim=$s";
+    `$cmd`;
+
+    # Throw one away
+    $f = @flavors[0];
+    $cmd = "bspline -A cpu -G 0 -f $f -M mse -m 0 -s \"15 15 15\" ./size_test_fixed.mha ./size_test_moving.mha";
+
+    foreach $repeat (0..2) {
     foreach $f (@flavors) {
 	$cmd = "bspline -A cpu -G 0 -f $f -M mse -m 1 -s \"15 15 15\" ./size_test_fixed.mha ./size_test_moving.mha";
 	open (CMD, "$cmd|");
@@ -20,6 +26,7 @@ foreach $s (@sizes) {
 	    $timer = $1;
 	}
 	print "$s,$f,$timer\n";
+    }
     }
 }
 
