68f3997d76bba02413e043f28b5256f4666e10c5
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2015-02-15 23:58:20 +0000

    Delete some obsolete files to make buggy debian tools happy

--- extra/debian/run_debuild.pl
+++ extra/debian/run_debuild.pl
@@ -3,20 +3,20 @@
 $pkg = "plastimatch";
 
 # Find version number
-$wc = `ls *.orig.tar.gz | wc -l`;
+$wc = `ls *.orig.tar.bz2 | wc -l`;
 chomp ($wc);
-($wc == 1) || die "Sorry, you need one (and only one) orig.tar.gz file\n";
-$orig_source = `ls *.orig.tar.gz`;
+($wc == 1) || die "Sorry, you need one (and only one) orig.tar.bz2 file\n";
+$orig_source = `ls *.orig.tar.bz2`;
 chomp ($orig_source);
-if ($orig_source =~ /plastimatch_(.*)\.orig\.tar\.gz/) {
+if ($orig_source =~ /plastimatch_(.*)\.orig\.tar\.bz2/) {
     $ver = "$1";
     print "Plastimatch version is $1\n";
 } else {
-    die "Sorry, couldn't pattern match your orig.tar.gz file\n";
+    die "Sorry, couldn't pattern match your orig.tar.bz2 file\n";
 }
 
 system ("rm -rf ${pkg}-${ver}");
-system ("tar xvf ${pkg}_${ver}.orig.tar.gz");
+system ("tar xvf ${pkg}_${ver}.orig.tar.bz2");
 chdir ("${pkg}-${ver}");
 print "Changed directory to ${pkg}-${ver}\n";
 system ("cp -r ../trunk/debian .");
