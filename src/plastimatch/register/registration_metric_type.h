3256549eeaad940ab39474c28a3b5dde6ab79d5a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2015-06-10 18:09:24 +0000

    Add missing file

---
+++ src/plastimatch/register/registration_metric_type.h
@@ -0,0 +1,16 @@
+/* -----------------------------------------------------------------------
+   See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
+   ----------------------------------------------------------------------- */
+#ifndef _registration_metric_type_h_
+#define _registration_metric_type_h_
+
+enum Registration_metric_type {
+    REGISTRATION_METRIC_NONE,
+    REGISTRATION_METRIC_GM,
+    REGISTRATION_METRIC_MI,
+    REGISTRATION_METRIC_MI_MATTES,
+    REGISTRATION_METRIC_MSE,
+    REGISTRATION_METRIC_NMI
+};
+
+#endif
