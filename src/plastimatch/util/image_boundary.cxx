82ddb55ac84171bae8089615dd2d3c9b23eeb95a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2015-07-23 17:47:15 +0000

    Change behavior of image boundary calculation from ZERO_PADDING to EDGE_PADDING

--- src/plastimatch/util/image_boundary.cxx
+++ src/plastimatch/util/image_boundary.cxx
@@ -17,11 +17,134 @@
 class Image_boundary_private {
 public:
     Image_boundary_private () {
+        vbb = Image_boundary::EDGE_PADDING;
+//        vbb = Image_boundary::ZERO_PADDING;
     }
 public:
     UCharImageType::Pointer input_image;
     UCharImageType::Pointer output_image;
+    Image_boundary::Volume_boundary_behavior vbb;
+public:
+    void run ();
+protected:
+    unsigned char classify_zp (
+        const Volume::Pointer& vol_in,
+        const unsigned char *img_in,
+        plm_long i, plm_long j, plm_long k, plm_long v)
+    {
+        /* If not inside volume, then not on boundary */
+        if (!img_in[v]) {
+            return 0;
+        }
+
+        /* Non-zero edge pixels are boundary */
+        if (k == 0 || k == vol_in->dim[2]-1
+            || j == 0 || j == vol_in->dim[1]-1
+            || i == 0 || i == vol_in->dim[0]-1)
+        {
+            return 1;
+        }
+
+        /* Look for neighboring zero voxel in six-neighborhood */
+        if (img_in[volume_index (vol_in->dim, i-1, j, k)] == 0) {
+            return 1;
+        }
+        if (img_in[volume_index (vol_in->dim, i+1, j, k)] == 0) {
+            return 1;
+        }
+        if (img_in[volume_index (vol_in->dim, i, j-1, k)] == 0) {
+            return 1;
+        }
+        if (img_in[volume_index (vol_in->dim, i, j+1, k)] == 0) {
+            return 1;
+        }
+        if (img_in[volume_index (vol_in->dim, i, j, k-1)] == 0) {
+            return 1;
+        }
+        if (img_in[volume_index (vol_in->dim, i, j, k+1)] == 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+    unsigned char classify_ep (
+        const Volume::Pointer& vol_in,
+        const unsigned char *img_in,
+        plm_long i, plm_long j, plm_long k, plm_long v)
+    {
+        /* If not inside volume, then not on boundary */
+        if (!img_in[v]) {
+            return 0;
+        }
+
+        /* Look for neighboring zero voxel in six-neighborhood,
+           ignoring voxels beyond boundary */
+        if (i != 0 
+            && img_in[volume_index (vol_in->dim, i-1, j, k)] == 0)
+        {
+            return 1;
+        }
+        if (i != vol_in->dim[0]-1 
+            && img_in[volume_index (vol_in->dim, i+1, j, k)] == 0)
+        {
+            return 1;
+        }
+        if (j != 0 
+            && img_in[volume_index (vol_in->dim, i, j-1, k)] == 0)
+        {
+            return 1;
+        }
+        if (j != vol_in->dim[1]-1
+            && img_in[volume_index (vol_in->dim, i, j+1, k)] == 0)
+        {
+            return 1;
+        }
+        if (k != 0 
+            && img_in[volume_index (vol_in->dim, i, j, k-1)] == 0)
+        {
+            return 1;
+        }
+        if (k != vol_in->dim[2]-1
+            && img_in[volume_index (vol_in->dim, i, j, k+1)] == 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
 };
+
+void 
+Image_boundary_private::run ()
+{
+    /* Convert to Plm_image type */
+    Plm_image pli_in (this->input_image);
+    Volume::Pointer vol_in = pli_in.get_volume_uchar ();
+    unsigned char *img_in = (unsigned char*) vol_in->img;
+
+    /* Allocate output image */
+    Plm_image *pli_out = pli_in.clone ();
+    Volume::Pointer vol_out = pli_out->get_volume_uchar ();
+    unsigned char *img_out = (unsigned char*) vol_out->img;
+
+    /* Compute the boundary */
+    for (plm_long k = 0, v = 0; k < vol_in->dim[2]; k++) {
+        for (plm_long j = 0; j < vol_in->dim[1]; j++) {
+            for (plm_long i = 0; i < vol_in->dim[0]; i++, v++) {
+                if (this->vbb == Image_boundary::ZERO_PADDING) {
+                    img_out[v] = classify_zp (vol_in, img_in, i, j, k, v);
+                } else {
+                    img_out[v] = classify_ep (vol_in, img_in, i, j, k, v);
+                }
+            }
+        }
+    }
+
+    /* Save the output image */
+    this->output_image = pli_out->itk_uchar ();
+
+    /* Clean up */
+    delete pli_out;
+}
 
 Image_boundary::Image_boundary ()
 {
@@ -49,72 +172,7 @@ Image_boundary::set_input_image (
 void 
 Image_boundary::run ()
 {
-    /* Convert to Plm_image type */
-    Plm_image pli_in (d_ptr->input_image);
-    Volume::Pointer vol_in = pli_in.get_volume_uchar ();
-    unsigned char *img_in = (unsigned char*) vol_in->img;
-
-    /* Allocate output image */
-    Plm_image *pli_out = pli_in.clone ();
-    Volume::Pointer vol_out = pli_out->get_volume_uchar ();
-    unsigned char *img_out = (unsigned char*) vol_out->img;
-
-    /* Compute the boundary */
-    for (plm_long k = 0, v = 0; k < vol_in->dim[2]; k++) {
-        for (plm_long j = 0; j < vol_in->dim[1]; j++) {
-            for (plm_long i = 0; i < vol_in->dim[0]; i++, v++)
-            {
-                /* Not a boundary unless one of the tests succeed */
-                img_out[v] = 0;
-
-                /* Only consider non-zero voxels */
-                if (!img_in[v]) {
-                    continue;
-                }
-
-                /* Non-zero edge pixels are boundary */
-                if (k == 0 || k == vol_in->dim[2]-1
-                    || j == 0 || j == vol_in->dim[1]-1
-                    || i == 0 || i == vol_in->dim[0]-1)
-                {
-                    img_out[v] = 1;
-                    continue;
-                }
-
-                /* Look for neighboring zero voxel in six-neighborhood */
-                if (img_in[volume_index (vol_in->dim, i-1, j, k)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-                if (img_in[volume_index (vol_in->dim, i+1, j, k)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-                if (img_in[volume_index (vol_in->dim, i, j-1, k)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-                if (img_in[volume_index (vol_in->dim, i, j+1, k)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-                if (img_in[volume_index (vol_in->dim, i, j, k-1)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-                if (img_in[volume_index (vol_in->dim, i, j, k+1)] == 0) {
-                    img_out[v] = 1;
-                    continue;
-                }
-            }
-        }
-    }
-
-    /* Save the output image */
-    d_ptr->output_image = pli_out->itk_uchar ();
-
-    /* Clean up */
-    delete pli_out;
+    d_ptr->run ();
 }
 
 UCharImageType::Pointer
