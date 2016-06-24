8d23d44aa8fe035e4f1144e550ed5cfbf86daca6
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2016-06-24 00:49:57 +0000

    Move package samples to extra directory

---
+++ extra/windows-install/launch_cmd_prompt.bat
@@ -0,0 +1,24 @@
+set target_file_a=C:\Windows\system32\cmd.exe
+@echo off
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%ALLUSERSPROFILE%\Desktop\plastimatch_cmd.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath =  "%target_file_a%" >> %SCRIPT%
+echo oLink.WorkingDirectory = "%~dp0" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+set SCRIPT2="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT2%
+echo sLinkFile = "%~dp0\plastimatch_cmd.lnk" >> %SCRIPT2%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT2%
+echo oLink.TargetPath =  "%target_file_a%" >> %SCRIPT2%
+echo oLink.WorkingDirectory = "%~dp0" >> %SCRIPT2%
+echo oLink.Save >> %SCRIPT2%
+cscript /nologo %SCRIPT2%
+del %SCRIPT2%
+
+start "" http://plastimatch.org/contents.html
+%SystemRoot%\explorer.exe "%~dp0"
