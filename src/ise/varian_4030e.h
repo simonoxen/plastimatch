1f7631a478a35b8030bc27321991bd1982d5f75a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2017-09-16 17:29:20 -0400

    Fix spacing

--- src/ise/varian_4030e.h
+++ src/ise/varian_4030e.h
@@ -19,7 +19,7 @@ class Dips_panel;
 struct IMGINFO
 {
 	double meanVal;
-	double SD;
+    double SD;
 	double minVal;
 	double maxVal;
 	quint16 chkSum;
@@ -35,7 +35,7 @@ public:
 
     int open_link (int panelIdx, const char *path);
     int check_link(int MaxTryCnt);
-	int disable_missing_corrections (int result);	
+    int disable_missing_corrections (int result);	
 
     void close_link ();	
 
@@ -45,12 +45,12 @@ public:
 
     int query_prog_info (UQueryProgInfo &crntStatus, bool showAll = FALSE);
    
-	int get_image_to_buf (int xSize, int ySize); 
-	int DBG_get_image_to_buf (unsigned short* pImgData, int xSize, int ySize); //receives external image buffer
-	bool CopyFromBufAndSendToDips (Dips_panel *dp); //get cur image to curImage
+    int get_image_to_buf (int xSize, int ySize); 
+    int DBG_get_image_to_buf (unsigned short* pImgData, int xSize, int ySize); //receives external image buffer
+    bool CopyFromBufAndSendToDips (Dips_panel *dp); //get cur image to curImage
 
-	void CalcImageInfo (double& meanVal, double& STDV, double& minVal, double& maxVal, int sizeX, int sizeY, USHORT* image_ptr); //all of the images taken before will be inspected		
-	bool SameImageExist(IMGINFO& curInfo, int& sameImgIndex);
+    void CalcImageInfo (double& meanVal, double& STDV, double& minVal, double& maxVal, int sizeX, int sizeY, USHORT* image_ptr); //all of the images taken before will be inspected		
+    bool SameImageExist(IMGINFO& curInfo, int& sameImgIndex);
 
     int idx;
     int current_mode;
@@ -61,19 +61,7 @@ public:
     int m_iSizeX;
     int m_iSizeY;    
 
-	std::vector<IMGINFO> m_vImageInfo; //to audit image duplication based on MEAN and SD
-
-	/* Below are orignal codes from Varian sample code. They are similarly implemented in acquire4030e_child class*/
-	// int wait_on_complete (UQueryProgInfo &crntStatus, int timeoutMsec = 0);
-	// int wait_on_num_frames (UQueryProgInfo &crntStatus, int numRequested, int timeoutMsec = 0);
-	//int wait_on_num_pulses (UQueryProgInfo &crntStatus, int timeoutMsec = 0);
-	//int wait_on_ready_for_pulse (UQueryProgInfo &crntStatus, int timeoutMsec = 0, int expectedState = TRUE);      
-	//int get_image_to_file (int xSize, int ySize, char *filename, int imageType = VIP_CURRENT_IMAGE);
-	// int get_image_to_dips (Dips_panel *dp, int xSize, int ySize); --> replaced by CopyFromBufAndSendToDips
-
-	//int SelectReceptor (); --> wrapping func. for vip_select_receptor
-
-	//quint16 GetCheckSum (unsigned short* pImage, int width, int height); --> Not working..currently
+    std::vector<IMGINFO> m_vImageInfo; //to audit image duplication based on MEAN and SD
 
 };
 #endif
