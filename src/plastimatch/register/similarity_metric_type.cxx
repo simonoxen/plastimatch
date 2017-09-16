06ba20259c0763380e9681d9f1ae941c9e6e2ad0
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2017-09-16 17:30:51 -0400

    Fix missing metric type

--- src/plastimatch/register/similarity_metric_type.cxx
+++ src/plastimatch/register/similarity_metric_type.cxx
@@ -12,6 +12,8 @@ similarity_metric_type_string (Similarity_metric_type type)
     switch (type) {
     case SIMILARITY_METRIC_NONE:
         return "none";
+    case SIMILARITY_METRIC_DMAP:
+        return "DMAP";
     case SIMILARITY_METRIC_GM:
         return "GM";
     case SIMILARITY_METRIC_MI_MATTES:
