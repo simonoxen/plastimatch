bb1365bccd54914054b6da4941918a62e3593e8f
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2017-10-17 16:51:58 -0400

    Separate out parts of YK16GrayImage that depends on ITK into separate files

--- src/ise/YK16GrayImage.cxx
+++ src/ise/YK16GrayImage.cxx
@@ -4,7 +4,6 @@
 #include <QLabel>
 #include <math.h>
 #include <QPainter>
-#include "itkImageRegionIterator.h"
 
 using namespace std;
 
@@ -409,74 +408,6 @@ bool YK16GrayImage::DoPixelReplacement(vector<BADPIXELMAP>& vPixelMapping )
 //		i++;
 //	}	
 //}
-
-void YK16GrayImage::CopyYKImage2ItkImage(YK16GrayImage* pYKImage, UnsignedShortImageType::Pointer& spTarImage)
-{
-	if (pYKImage == NULL)
-		return;
-	//Raw File open	
-	//UnsignedShortImageType::SizeType tmpSize = 
-	UnsignedShortImageType::RegionType region = spTarImage->GetRequestedRegion();
-	UnsignedShortImageType::SizeType tmpSize = region.GetSize();
-
-	int sizeX = tmpSize[0];
-	int sizeY = tmpSize[1];
-
-	if (sizeX < 1 || sizeY <1)
-		return;
-
-	itk::ImageRegionIterator<UnsignedShortImageType> it(spTarImage, region);
-
-	int i = 0;
-	for (it.GoToBegin() ; !it.IsAtEnd(); ++it)
-	{
-		it.Set(pYKImage->m_pData[i]);
-		i++;
-	}
-	//int totCnt = i;
-	//writerType::Pointer writer = writerType::New();	
-	//writer->SetInput(spTarImage);	
-	//writer->SetFileName("C:\\ThisImageIs_spSrcImage.png");	//It works!
-	//writer->Update();
-}
-void YK16GrayImage::CopyItkImage2YKImage(UnsignedShortImageType::Pointer& spSrcImage, YK16GrayImage* pYKImage)
-{
-	if (pYKImage == NULL)
-		return;
-	//Raw File open	
-	//UnsignedShortImageType::SizeType tmpSize = 
-	UnsignedShortImageType::RegionType region = spSrcImage->GetRequestedRegion();
-	UnsignedShortImageType::SizeType tmpSize = region.GetSize();
-
-	int sizeX = tmpSize[0];
-	int sizeY = tmpSize[1];
-
-	if (sizeX < 1 || sizeY <1)
-		return;
-
-	//itk::ImageRegionConstIterator<UnsignedShortImageType> it(spSrcImage, region);
-	itk::ImageRegionIterator<UnsignedShortImageType> it(spSrcImage, region);
-
-	int i = 0;
-	for (it.GoToBegin() ; !it.IsAtEnd() ; ++it)
-	{
-		pYKImage->m_pData[i] = it.Get();
-		i++;
-	}
-	//int totCnt = i; //Total Count is OK
-
-	//int width = pYKImage->m_iWidth;
-	//int height = pYKImage->m_iHeight;
-
-
-	//writerType::Pointer writer = writerType::New();	
-	//writer->SetInput(spSrcImage);	
-	//writer->SetFileName("C:\\ThisImageIs_spSrcImage2.png");	//It works!
-	//writer->Update();
-}
-
-
-
 
 
 bool YK16GrayImage::CalcImageInfo ()
