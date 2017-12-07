9730156d6d36e6035cf886cff7b3840aa75d8da1
Gregory Sharp <gregsharp.geo@yahoo.com>
2017-12-07 15:16:13 -0500

    Fix DCMTK-related build problem on windows

--- cmake/FindOpenMP.cmake
+++ cmake/FindOpenMP.cmake
@@ -1,16 +1,33 @@
 ######################################################
 ##  OpenMP
 ######################################################
-INCLUDE(CheckFunctionExists)
+include (CheckFunctionExists)
+include (CheckCXXSourceCompiles)
 MESSAGE(STATUS "Check for compiler OpenMP support...")
 SET(OPENMP_FLAGS)
 SET(OPENMP_LIBRARIES)
 SET(OPENMP_FOUND FALSE)
 
+# sample openmp source code to test
+set(OpenMP_C_TEST_SOURCE
+"
+#include <omp.h>
+int main() {
+#ifdef _OPENMP
+  return 0;
+#else
+  breaks_on_purpose
+#endif
+}
+")
+
 # Key: CFLAGS##LDFLAGS#LIBRARIES
 # Neither CFLAGS nor LDFLAGS can be empty.  Use NONE instead.
 SET(
   OPENMP_FLAGS_AND_LIBRARIES
+  # MSVC - this should be the first one, since the below test based on 
+  # function availability is insufficient on that platform
+  "/openmp##NONE#"
   # gcc
   "-fopenmp##-fopenmp#"
   "-fopenmp##-fopenmp#gomp"
@@ -28,8 +45,6 @@ SET(
   "-omp##-omp#"
   # AIX
   "-qsmp=omp##-qsmp=omp#"
-  # MSVC
-  "/openmp##NONE#"
 )
 
 # Massive hack to workaround CMake limitations
@@ -42,10 +57,10 @@ FOREACH(I RANGE 0 ${NUM_FLAGS})
     STRING(REGEX REPLACE "[^#]*##" "" TMP ${TMP})
     STRING(REGEX MATCH "([^#]*)" OPENMP_LDFLAGS ${TMP})
     STRING(REGEX REPLACE "[^#]*#" "" OPENMP_LIBRARIES ${TMP})
-    #MESSAGE(STATUS "OPENMP_FLAGS=${OPENMP_FLAGS}")
-    #MESSAGE(STATUS "OPENMP_LDFLAGS = ${OPENMP_LDFLAGS}")
-    #MESSAGE(STATUS "OPENMP_LIBRARIES = ${OPENMP_LIBRARIES}")
-    #MESSAGE(STATUS "-------")
+    # MESSAGE(STATUS "OPENMP_FLAGS=${OPENMP_FLAGS}")
+    # MESSAGE(STATUS "OPENMP_LDFLAGS = ${OPENMP_LDFLAGS}")
+    # MESSAGE(STATUS "OPENMP_LIBRARIES = ${OPENMP_LIBRARIES}")
+    # MESSAGE(STATUS "-------")
 
     IF(OPENMP_LDFLAGS MATCHES "NONE")
       SET(OPENMP_LDFLAGS "")
@@ -54,12 +69,19 @@ FOREACH(I RANGE 0 ${NUM_FLAGS})
       STRING(REPLACE " " ";" OPENMP_LIBRARIES ${OPENMP_LIBRARIES})
     ENDIF(OPENMP_LIBRARIES MATCHES " ")
 
+    set (SRC_FILE ${CMAKE_BINARY_DIR}/omptest.cpp)
+    file (WRITE ${SRC_FILE} "${OpenMP_C_TEST_SOURCE}")
+
     ## I think I need to do a try-compile
     SET(CMAKE_REQUIRED_FLAGS_BACKUP ${CMAKE_REQUIRED_FLAGS})
     SET(CMAKE_REQUIRED_LIBRARIES_BACKUP ${CMAKE_REQUIRED_LIBRARIES})
-    SET(CMAKE_REQUIRED_FLAGS ${OPENMP_FLAGS})
+    list (APPEND CMAKE_REQUIRED_FLAGS ${OPENMP_FLAGS})
     SET(CMAKE_REQUIRED_LIBRARIES ${OPENMP_LIBRARIES})
-    CHECK_FUNCTION_EXISTS(omp_get_thread_num OPENMP_FOUND${I})
+
+    # CHECK_FUNCTION_EXISTS(omp_get_thread_num OPENMP_FOUND${I})
+    # check_cxx_source_compiles("${OpenMP_CXX_TEST_SOURCE}" OPENMP_FOUND${I})
+    # message (STATUS "OPENMP: ${CMAKE_REQUIRED_FLAGS} | ${CMAKE_REQUIRED_LIBRARIES} | ${OPENMP_FOUND${I}}")
+
     SET(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS_BACKUP})
     SET(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_BACKUP})
 
