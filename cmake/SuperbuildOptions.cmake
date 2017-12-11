3e57155b7851d6e2b7f20f328c012066fd2b3ed5
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2017-12-11 18:13:49 -0500

    Add capability for inner build to access root of source and binary trees

--- cmake/SuperbuildOptions.cmake
+++ cmake/SuperbuildOptions.cmake
@@ -1,9 +1,26 @@
 ##-----------------------------------------------------------------------------
 ##  See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
 ##-----------------------------------------------------------------------------
+
 # macro: sb_option
-# Create an option in the cmake-gui that is passed to a nested superbuild
+# Create an option in the cmake-gui, and mark the variable as a superbuild
+# variable to be passed to the inner build
 macro (sb_option _var _desc _defval)
   option (${_var} ${_desc} ${_defval})
+  list (APPEND sb_cmake_vars ${_var})
+endmacro ()
+
+# macro: sb_set
+# Set a variable and add it to the list of superbuild variables
+# that are passed to the inner build
+macro (sb_set _var _val)
+  set (${_var} ${_val})
+  list (APPEND sb_cmake_vars ${_var})
+endmacro ()
+
+# macro: sb_variable
+# Add a variable to the list of superbuild variables that are passed to the
+# inner build
+macro (sb_variable _var)
   list (APPEND sb_cmake_vars ${_var})
 endmacro ()
