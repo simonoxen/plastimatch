e1392a97c467d12e413c9c5e65ecc0ced058d98a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2017-12-13 14:13:21 -0500

    Superbuild now working on linux

--- cmake/SuperbuildOptions.cmake
+++ cmake/SuperbuildOptions.cmake
@@ -1,7 +1,10 @@
 ##-----------------------------------------------------------------------------
 ##  See COPYRIGHT.TXT and LICENSE.TXT for copyright and license information
 ##-----------------------------------------------------------------------------
-
+##  These macros are used to forward variables defined in the outer build
+##  to the inner build.  The variable values cannot be a list; these
+##  don't expand properly.  
+##-----------------------------------------------------------------------------
 # macro: sb_variable
 # Add a variable to the list of superbuild variables that are passed to the
 # inner build
@@ -12,8 +15,8 @@ endmacro ()
 # macro: sb_set
 # Set a variable and add it to the list of superbuild variables
 # that are passed to the inner build
-macro (sb_set _var _val)
-  set (${_var} ${_val})
+macro (sb_set _var)
+  set (${_var} ${ARGN})
   list (APPEND sb_cmake_vars ${_var})
 endmacro ()
 
