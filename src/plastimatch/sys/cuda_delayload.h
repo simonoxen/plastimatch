c11a1056a80c3a2f012ad545c5b726e5a315f245
Gregory Sharp <gregsharp@geocities.com>
2018-01-11 11:50:19 -0500

    Writeup notes on how delayload works

--- src/plastimatch/sys/cuda_delayload.h
+++ src/plastimatch/sys/cuda_delayload.h
@@ -19,12 +19,12 @@
 // Note: if lib contains a null pointer here
 //       (see above note), this will return a
 //       null function pointer.  Be careful pls.
-#if !defined(_WIN32) && defined(PLM_USE_GPU_PLUGINS)
-    #define LOAD_SYMBOL(sym, lib)                  \
-        sym##_##t* sym = (sym##_##t*) dlsym (lib, #sym);          
-#else
+#if _WIN32
     #define LOAD_SYMBOL(sym, lib)                  \
         ;
+#else
+    #define LOAD_SYMBOL(sym, lib)                  \
+        sym##_##t* sym = (sym##_##t*) dlsym (lib, #sym);
 #endif
 
 // JAS 2012.03.29
@@ -32,14 +32,14 @@
 // Now that plastimatch is officially C++, we can now safely
 // define this macro, which reduces programmer error.  This
 // should be used instead of LOAD_LIBRARY
-#if !defined(_WIN32) && defined(PLM_USE_GPU_PLUGINS)
-    #define LOAD_LIBRARY_SAFE(lib)                 \
-        if (!delayload_##lib()) { exit (0); }      \
-        void* lib = dlopen_ex (#lib".so");          
-#else
+#if _WIN32
     #define LOAD_LIBRARY_SAFE(lib)                 \
         if (!delayload_##lib()) { exit (0); }      \
         ;
+#else
+    #define LOAD_LIBRARY_SAFE(lib)                 \
+        if (!delayload_##lib()) { exit (0); }      \
+        void* lib = dlopen_ex (#lib".so");
 #endif
 
 // JAS 2010.12.09
@@ -49,19 +49,19 @@
     #define UNLOAD_LIBRARY(lib)                    \
         if (lib != NULL) {                         \
             dlclose (lib);                         \
-        }                                           
+        }
 #else
     #define UNLOAD_LIBRARY(lib)                    \
         ;
 #endif
 
-#define DELAYLOAD_WRAP(f, ...)                          \
+#define DELAYLOAD_WRAP(f, ...)                     \
     f (__VA_ARGS__); typedef f##_t(__VA_ARGS__);
 
 PLMSYS_C_API int delayload_libplmcuda (void);
 PLMSYS_C_API int delayload_libplmreconstructcuda (void);
 PLMSYS_C_API int delayload_libplmregistercuda (void);
 PLMSYS_C_API int delayload_libplmopencl (void);
-PLMSYS_C_API void* dlopen_ex (const char* lib); 
+PLMSYS_C_API void* dlopen_ex (const char* lib);
 
 #endif
