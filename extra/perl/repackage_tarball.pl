8e30ad58eb73d92572d5f7ba06e263f52584f7fa
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2018-01-26 13:30:11 -0500

    Add perl script for repackaging tarball

---
+++ extra/perl/repackage_tarball.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/env perl
+
+use File::Copy;
+use File::Path 'remove_tree';
+
+opendir (DIR, ".") or die $!;
+while (my $f = readdir (DIR)) {
+    chomp ($f);
+    if ($f =~ m/plastimatch-(.*)-.*\.tar\.gz/ or
+	$f =~ m/plastimatch-(.*)-.*\.tar\.bz2/) {
+	$tarball_name = $f;
+	$version = $1;
+	print "Repackaging version $version\n";
+	print "File is $f\n";
+	last;
+    }
+}
+closedir (DIR);
+
+if (not defined $version) {
+    die "No appropriate file found.\n";
+}
+
+system ("tar xvf $tarball_name");
+
+$tarball_name =~ m/(.*)\.tar.*/;
+$dirname = $1;
+if (not -d $dirname) {
+    die "Directory not found after unpacking tarball.\n";
+}
+
+$dirname =~ m/(.*)-.*/; $new_dirname = $1; if (-z $new_dirname) { die
+"New directory string was empty.\n"; }
+
+move ($dirname, $new_dirname);
+
+$new_tarball_name = "${new_dirname}.tar.bz2";
+system ("tar cjvf $new_tarball_name $new_dirname");
+
+remove_tree (${new_dirname});
+
