d458b41a90e16992ca1900f920e8612fe3b358c8
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2019-04-30 08:08:45 -0400

    Fix build problem on windows

--- cmake/FindDCMTK_wrap.cmake
+++ cmake/FindDCMTK_wrap.cmake
@@ -8,16 +8,10 @@ if (DCMTK_wrap_FIND_REQUIRED)
   list (APPEND DCMTK_EXTRA_ARGS REQUIRED)
 endif ()
 
-# if (DCMTK_DIR AND EXISTS "${DCMTK_DIR}/DCMTKConfig.cmake")
-#   find_package (DCMTK NO_MODULE ${EXTRA_ARGS})
-# else ()
-#   find_package (DCMTK_legacy ${EXTRA_ARGS})
-# endif ()
-
-find_package (DCMTK NO_MODULE ${EXTRA_ARGS})
+find_package (DCMTK NO_MODULE ${DCMTK_EXTRA_ARGS})
 if (NOT DCMTK_FOUND)
   message (STATUS "Searching for DCMTK using legacy method")
-  find_package (DCMTK_legacy ${EXTRA_ARGS})
+  find_package (DCMTK_legacy ${DCMTK_EXTRA_ARGS})
 endif ()
 
 # The DCMTK 3.6.2 DCMTKConfig.cmake seems to be broken on windows
