b749779f28a301880656b7ab6346eee6aaa82092
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2019-08-29 10:45:25 -0400

    Add more verbose information to cuda_probe

--- src/plastimatch/cuda/cuda_probe.cu
+++ src/plastimatch/cuda/cuda_probe.cu
@@ -13,6 +13,15 @@ cuda_probe (void)
 {
     int devicecount = -1;
     printf ("Testing for CUDA...\n");
+
+    int driver_version = 0, runtime_version = 0;
+    cudaRuntimeGetVersion(&runtime_version);
+    cudaDriverGetVersion(&driver_version);
+    printf ("CUDA driver version: %d.%d\n",
+        driver_version / 1000, (driver_version % 100)/10);
+    printf ("CUDA runtime version: %d.%d\n",
+        runtime_version / 1000, (runtime_version % 100)/10);
+    
     cudaError_t rc = cudaGetDeviceCount (&devicecount);
     if (rc != cudaSuccess) {
 	printf("Call to cudaGetDeviceCount() returned failure (%d)\n",
@@ -30,12 +39,13 @@ cuda_probe (void)
     cudaDeviceProp props;
     cudaGetDeviceProperties (&props, 0);
     printf ("Compute Capability %d.%d\n", props.major, props.minor);
-    if (props.major >= 1) {
-    	printf ("Device is presumed cuda capable.\n");
-    	return true;
+    if (props.major < 1) {
+        printf ("Device is presumed NOT cuda capable.\n");
+        return false;
     }
 
+    printf ("Device is presumed cuda capable.\n");
+    return true;
     // Unless proven otherwise, we assume no CUDA.
     printf ("Device is presumed NOT cuda capable.\n");
-    return false;
 }
