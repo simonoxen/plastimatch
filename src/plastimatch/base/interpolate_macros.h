a520f2ba2badc4ea956d1e6b5626960ea3c2db71
Keyur Shah <keyurs19@gmail.com>
2020-01-09 17:27:12 -0500

    Add temporary source code for flavor o to calculate mse

--- src/plastimatch/base/interpolate_macros.h
+++ src/plastimatch/base/interpolate_macros.h
@@ -37,6 +37,57 @@
 		+ m_x1y1z2 + m_x2y1z2 + m_x1y2z2 + m_x2y2z2;		\
     } while (0)
 
+#define LI_VALUE_x(m_val, fx1, fx2, fy1, fy2, fz1, fz2, mvf, m_img, moving) \
+    do {								\
+	float m_x1y1z1, m_x2y1z1, m_x1y2z1, m_x2y2z1;			\
+	float m_x1y1z2, m_x2y1z2, m_x1y2z2, m_x2y2z2;			\
+									\
+	m_x1y1z1 = fx1 * fy1 * fz1 * m_img[mvf];			\
+	m_x2y1z1 = fx2 * fy1 * fz1 * m_img[mvf+1];			\
+	m_x1y2z1 = fx1 * fy2 * fz1 * m_img[mvf+moving->dim[0]];		\
+	m_x2y2z1 = fx2 * fy2 * fz1 * m_img[mvf+moving->dim[0]+1];	\
+	m_x1y1z2 = fx1 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]]; \
+	m_x2y1z2 = fx2 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+1]; \
+	m_x1y2z2 = fx1 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]]; \
+	m_x2y2z2 = fx2 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]+1]; \
+	m_val = m_x1y1z1 + m_x2y1z1 + m_x1y2z1 + m_x2y2z1		\
+		+ m_x1y1z2 + m_x2y1z2 + m_x1y2z2 + m_x2y2z2;		\
+    } while (0)
+
+#define LI_VALUE_y(m_val, fx1, fx2, fy1, fy2, fz1, fz2, mvf, m_img, moving) \
+    do {								\
+	float m_x1y1z1, m_x2y1z1, m_x1y2z1, m_x2y2z1;			\
+	float m_x1y1z2, m_x2y1z2, m_x1y2z2, m_x2y2z2;			\
+									\
+	m_x1y1z1 = fx1 * fy1 * fz1 * m_img[mvf];			\
+	m_x2y1z1 = fx2 * fy1 * fz1 * m_img[mvf+1];			\
+	m_x1y2z1 = fx1 * fy2 * fz1 * m_img[mvf+moving->dim[0]];		\
+	m_x2y2z1 = fx2 * fy2 * fz1 * m_img[mvf+moving->dim[0]+1];	\
+	m_x1y1z2 = fx1 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]]; \
+	m_x2y1z2 = fx2 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+1]; \
+	m_x1y2z2 = fx1 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]]; \
+	m_x2y2z2 = fx2 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]+1]; \
+	m_val = m_x1y1z1 + m_x2y1z1 + m_x1y2z1 + m_x2y2z1		\
+		+ m_x1y1z2 + m_x2y1z2 + m_x1y2z2 + m_x2y2z2;		\
+    } while (0)
+
+#define LI_VALUE_z(m_val, fx1, fx2, fy1, fy2, fz1, fz2, mvf, m_img, moving) \
+    do {								\
+	float m_x1y1z1, m_x2y1z1, m_x1y2z1, m_x2y2z1;			\
+	float m_x1y1z2, m_x2y1z2, m_x1y2z2, m_x2y2z2;			\
+									\
+	m_x1y1z1 = fx1 * fy1 * fz1 * m_img[mvf];			\
+	m_x2y1z1 = fx2 * fy1 * fz1 * m_img[mvf+1];			\
+	m_x1y2z1 = fx1 * fy2 * fz1 * m_img[mvf+moving->dim[0]];		\
+	m_x2y2z1 = fx2 * fy2 * fz1 * m_img[mvf+moving->dim[0]+1];	\
+	m_x1y1z2 = fx1 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]]; \
+	m_x2y1z2 = fx2 * fy1 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+1]; \
+	m_x1y2z2 = fx1 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]]; \
+	m_x2y2z2 = fx2 * fy2 * fz2 * m_img[mvf+moving->dim[1]*moving->dim[0]+moving->dim[0]+1]; \
+	m_val = m_x1y1z1 + m_x2y1z1 + m_x1y2z1 + m_x2y2z1		\
+		+ m_x1y1z2 + m_x2y1z2 + m_x1y2z2 + m_x2y2z2;		\
+    } while (0)
+
 #define LI_VALUE_ALT(m_val, fx1, fx2, fy1, fy2, fz1, fz2,		\
     mvf, m_img, moving, plane)						\
     do {								\
