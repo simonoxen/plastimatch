d285ef8daa833713a5cc73d609b417baeb3a908a
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2020-04-23 12:43:05 -0400

    Work on getting emacs call graph to work

--- cmake/FindEtags.cmake
+++ cmake/FindEtags.cmake
@@ -1,7 +1,9 @@
 ## From Jan Woetzel, CMAKE email list
 ## http://www.cmake.org/pipermail/cmake/2006-January/007883.html
 IF (UNIX)
-  ADD_CUSTOM_TARGET(tags etags --members --declarations  `find ${CMAKE_SOURCE_DIR} -name *.cc -or -name *.cxx -or -name *.hxx -or -name *.hh -or -name *.cpp -or -name *.h -or -name *.c -or -name *.f`)
-  ADD_CUSTOM_TARGET(etags DEPENDS tags)
+  ADD_CUSTOM_TARGET(etags etags --members --declarations  `find ${CMAKE_SOURCE_DIR} -name *.cc -or -name *.cxx -or -name *.hxx -or -name *.hh -or -name *.cpp -or -name *.h -or -name *.c -or -name *.f`)
+  ADD_CUSTOM_TARGET(gtags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} -E env "GTAGSFORCECPP=1" gtags)
   MESSAGE (STATUS "Etags targets added.")
 ENDIF (UNIX)
