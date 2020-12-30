8f4f1ebe7535a193b818acaa7d8e5156b99495df
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2020-12-30 09:42:07 -0500

    Update for building on Visual Studio 2015

--- src/plastimatch/sys/smart_pointer.h
+++ src/plastimatch/sys/smart_pointer.h
@@ -35,32 +35,32 @@
     static T::Pointer New () {                                  \
         return T::Pointer (new T);                              \
     }                                                           \
-    static T::Pointer New (T* t) {                              \
+    static typename T::Pointer New (T* t) {                     \
         return T::Pointer (t);                                  \
     }                                                           \
     template<class U1                                           \
-             > static T::Pointer New (                          \
-                 const U1& u1) {                                \
+        > static typename T::Pointer New (                      \
+            const U1& u1) {                                     \
         return T::Pointer (                                     \
             new T(u1));                                         \
     }                                                           \
     template<class U1, class U2                                 \
-             > static T::Pointer New (                          \
-                 const U1& u1, const U2& u2) {                  \
+        > static typename T::Pointer New (                      \
+            const U1& u1, const U2& u2) {                       \
         return T::Pointer (                                     \
             new T(u1, u2));                                     \
     }                                                           \
     template<class U1, class U2, class U3                       \
-             > static T::Pointer New (                          \
-                 const U1& u1, const U2& u2, const U3& u3) {    \
+        > static typename T::Pointer New (                      \
+            const U1& u1, const U2& u2, const U3& u3) {         \
         return T::Pointer (                                     \
             new T(u1, u2, u3));                                 \
     }                                                           \
     template<class U1, class U2, class U3,                      \
              class U4, class U5, class U6                       \
-             > static T::Pointer New (                          \
-                 const U1& u1, const U2& u2, const U3& u3,      \
-                 const U4& u4, const U5& u5, const U6& u6) {    \
+        > static typename T::Pointer New (                      \
+            const U1& u1, const U2& u2, const U3& u3,           \
+            const U4& u4, const U5& u5, const U6& u6) {         \
         return T::Pointer (                                     \
             new T(u1, u2, u3, u4, u5, u6));                     \
     }
