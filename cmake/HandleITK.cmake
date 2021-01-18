simonoxen/plastimatch

commit 6ec03f6d9630111ac04eccaa308f4df72a2bf144

Finally I must accept the fact that Fedora's ITK package is broken.

cmake/HandleITK.cmake
@@ -57,6 +57,27 @@ set (CMAKE_REQUIRED_INCLUDES "${ITK_INCLUDE_DIRS}")
 check_include_file_cxx ("itkVectorCentralDifferenceImageFunction.h" HAVE_ITK_VECTOR_CD)
 pop_vars ("CMAKE_REQUIRED_INCLUDES")
 
+# See if itkArray.h can be included.  ITK 4 is broken on Fedora with gcc 10 compiler.
+# There is no way to fix this.
+set (ITK_TEST_SOURCE 
+  "#include <itkArray.h>
+   int main (int argc, char* argv[]) {return 0;}")
+push_vars ("CMAKE_REQUIRED_INCLUDES")
+set (CMAKE_REQUIRED_INCLUDES ${ITK_INCLUDE_DIRS})
+    set (CMAKE_REQUIRED_QUIET TRUE)
+check_cxx_source_compiles ("${ITK_TEST_SOURCE}" ITK_IS_OK)
+if (NOT ITK_IS_OK)
+  message (STATUS "ITK is broken. Sorry.")
+  set (ITK_FOUND OFF)
+endif ()
+pop_vars ("CMAKE_REQUIRED_INCLUDES")
+
+push_vars ("CMAKE_REQUIRED_INCLUDES")
+unset (HAVE_ITK_VECTOR_CD CACHE)
+set (CMAKE_REQUIRED_INCLUDES "${ITK_INCLUDE_DIRS}")
+check_include_file_cxx ("itkVectorCentralDifferenceImageFunction.h" HAVE_ITK_VECTOR_CD)
+pop_vars ("CMAKE_REQUIRED_INCLUDES")
+
 # For ITK 4, we need to override the default itkContourExtractor2DImageFilter
 # because it doesn't compile with newer gcc versions
 if ("${ITK_VERSION}" VERSION_LESS "5")

commit 242fd88b40204a988a19a5fc1b0dbbd683af2b34

COMP: Fix configuration against ITKv5 excluding compile test
This commit fixes a regression introduced in 6ec03f6d9 (Finally I must
accept the fact that Fedora's ITK package is broken.), since the test
integrated in the referenced commit is meaningful only when building
against ITK 4.x, it is explicitly excluded when building against ITK 5.0

cmake/HandleITK.cmake
@@ -57,20 +57,22 @@ set (CMAKE_REQUIRED_INCLUDES "${ITK_INCLUDE_DIRS}")
 check_include_file_cxx ("itkVectorCentralDifferenceImageFunction.h" HAVE_ITK_VECTOR_CD)
 pop_vars ("CMAKE_REQUIRED_INCLUDES")
 
-# See if itkArray.h can be included.  ITK 4 is broken on Fedora with gcc 10 compiler.
-# There is no way to fix this.
-set (ITK_TEST_SOURCE 
-  "#include <itkArray.h>
-   int main (int argc, char* argv[]) {return 0;}")
-push_vars ("CMAKE_REQUIRED_INCLUDES")
-set (CMAKE_REQUIRED_INCLUDES ${ITK_INCLUDE_DIRS})
-    set (CMAKE_REQUIRED_QUIET TRUE)
-check_cxx_source_compiles ("${ITK_TEST_SOURCE}" ITK_IS_OK)
-if (NOT ITK_IS_OK)
-  message (STATUS "ITK is broken. Sorry.")
-  set (ITK_FOUND OFF)
-endif ()
-pop_vars ("CMAKE_REQUIRED_INCLUDES")
+if (${ITK_VERSION} VERSION_LESS "5")
+  # See if itkArray.h can be included.  ITK 4 is broken on Fedora with gcc 10 compiler.
+  # There is no way to fix this.
+  set (ITK_TEST_SOURCE
+    "#include <itkArray.h>
+     int main (int argc, char* argv[]) {return 0;}")
+  push_vars ("CMAKE_REQUIRED_INCLUDES")
+  set (CMAKE_REQUIRED_INCLUDES ${ITK_INCLUDE_DIRS})
+      set (CMAKE_REQUIRED_QUIET TRUE)
+  check_cxx_source_compiles ("${ITK_TEST_SOURCE}" ITK_IS_OK)
+  if (NOT ITK_IS_OK)
+    message (STATUS "ITK is broken. Sorry.")
+    set (ITK_FOUND OFF)
+  endif ()
+  pop_vars ("CMAKE_REQUIRED_INCLUDES")
+endif()
 
 push_vars ("CMAKE_REQUIRED_INCLUDES")
 unset (HAVE_ITK_VECTOR_CD CACHE)